8afbe45ce1c936137e02163dfcef1e49f7846fc7
burgholzer <burgholzer@me.com>
2025-04-06 15:28:56 +0200

    🔧 actually require `pybind11>=2.13.6` in CMake
    Signed-off-by: burgholzer <burgholzer@me.com>

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -25,7 +25,7 @@ if(BUILD_MQT_CORE_BINDINGS)
   message(STATUS "Python executable: ${Python_EXECUTABLE}")
 
   # add pybind11 library
-  find_package(pybind11 2.13.5 CONFIG REQUIRED)
+  find_package(pybind11 2.13.6 CONFIG REQUIRED)
 endif()
 
 set(JSON_VERSION
