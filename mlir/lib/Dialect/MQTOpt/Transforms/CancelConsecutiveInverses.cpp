03cc6124e7ea39fc4a3e3877bfdc524bb001a41e
flowerthrower <flowerthrower@users.noreply.github.com>
2025-04-23 16:26:23 +0200

    🔧 Refactor include order in CancelConsecutiveInverses.cpp for consistency

--- mlir/lib/Dialect/MQTOpt/Transforms/CancelConsecutiveInverses.cpp
+++ mlir/lib/Dialect/MQTOpt/Transforms/CancelConsecutiveInverses.cpp
@@ -8,13 +8,13 @@
  * Licensed under the MIT License
  */
 
+#include "mlir/Dialect/Common/Compat.h"
 #include "mlir/Dialect/MQTOpt/Transforms/Passes.h"
 
 #include <mlir/IR/PatternMatch.h>
 #include <mlir/Support/LLVM.h>
 #include <mlir/Transforms/GreedyPatternRewriteDriver.h>
 #include <utility>
-#include "mlir/Dialect/Common/Compat.h"
 
 namespace mqt::ir::opt {
 
