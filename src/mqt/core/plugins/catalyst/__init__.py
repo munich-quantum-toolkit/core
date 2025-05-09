4973ae68ee4fb3ad169c35818275be1b8504a783
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-05-09 08:39:13 +0200

    🎨 Improve plugin's python script

--- src/mqt/core/plugins/catalyst/__init__.py
+++ src/mqt/core/plugins/catalyst/__init__.py
@@ -24,22 +24,39 @@
 
 from __future__ import annotations
 
-import platform
+from importlib.metadata import PackageNotFoundError, distribution
 from pathlib import Path
 
 import pennylane as qml
 from catalyst.passes import PassPlugin
 
 
-def getMQTPluginAbsolutePath():
+def get_catalyst_plugin_abs_path() -> Path:
     """Returns the absolute path to the MQT plugin."""
-    ext = "so" if platform.system() == "Linux" else "dylib"
-    return Path(Path(__file__).parent.absolute(), f"mqt-catalyst-plugin.{ext}")
+    try:
+        dist = distribution("mqt-core")
+        # Check for the plugin in the mqt-core package on Linux
+        catalyst_plugin_abs_path = Path(dist.locate_file("mqt/core/???/mqt-catalyst-plugin.so"))
+        if catalyst_plugin_abs_path.exists() and catalyst_plugin_abs_path.is_file():
+            return catalyst_plugin_abs_path
+        # Check for the plugin in the mqt-core package on macOS
+        catalyst_plugin_abs_path = Path(dist.locate_file("mqt/core/???/mqt-catalyst-plugin.dylib"))
+        if catalyst_plugin_abs_path.exists() and catalyst_plugin_abs_path.is_file():
+            return catalyst_plugin_abs_path
+        # Check for the plugin in the mqt-core package on Windows
+        catalyst_plugin_abs_path = Path(dist.locate_file("mqt/core/???/mqt-catalyst-plugin.dll"))
+        if catalyst_plugin_abs_path.exists() and catalyst_plugin_abs_path.is_file():
+            return catalyst_plugin_abs_path
+        msg = "mqt-catalyst-plugin library not found."
+        raise FileNotFoundError(msg)
+    except PackageNotFoundError:
+        msg = "mqt-core not installed, installation required to access the mqt-catalyst-plugin library."
+        raise ImportError(msg) from None
 
 
-def name2pass(_name):
+def name2pass(_name: str) -> tuple[Path, str]:
     """Example entry point for MQT plugin."""
-    return getMQTPluginAbsolutePath(), "mqt-core-round-trip"
+    return get_catalyst_plugin_abs_path(), "mqt-core-round-trip"
 
 
 def MQTCoreRoundTrip(*flags, **valued_options):
@@ -49,7 +66,7 @@ def MQTCoreRoundTrip(*flags, **valued_options):
         pass_pipeline = kwargs.get("pass_pipeline", [])
         pass_pipeline.append(
             PassPlugin(
-                getMQTPluginAbsolutePath(),
+                get_catalyst_plugin_abs_path(),
                 "mqt-core-round-trip",
                 *flags,
                 **valued_options,
