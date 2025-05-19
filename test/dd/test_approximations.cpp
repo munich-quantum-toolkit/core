e565d0fb5bc732b4da36af22ae35c0d51bae105a
Matthias Reumann <matthias.reumann@tum.de>
2025-05-19 15:51:08 +0200

    Add repeats to runtime tests.

--- test/dd/test_approximations.cpp
+++ test/dd/test_approximations.cpp
@@ -329,29 +329,32 @@ TEST(ApproximationTest, ThreeQubitRemoveUnconnected) {
 TEST(ApproximationTest, Runtime) {
   {
     constexpr std::size_t n = 15;         // Up to 16 qubits.
+    constexpr std::size_t repeats = 10;   // Repeat benchmark 10 times.
     constexpr double fidelity = 1 - 0.01; // Budget of .02
 
     std::array<std::size_t, n> qubits{}; // Qubit counts: [2, 16]
     std::iota(qubits.begin(), qubits.end(), 2);
 
     for (std::size_t i = 0; i < n; ++i) {
-      const std::size_t nq = qubits[i];
+      std::size_t nodes{};
+      double rt{};
 
+      const std::size_t nq = qubits[i];
       auto dd = std::make_unique<dd::Package>(nq);
 
-      auto state = generateExponentialDD(nq, *dd);
-      const std::size_t numNodes = state.size() - 1; // Minus terminal.
+      for (std::size_t r = 0; r < repeats; ++r) {
+        auto state = generateExponentialDD(nq, *dd);
+        nodes += state.size() - 1; // Minus terminal.
 
-      const auto t1 = std::chrono::high_resolution_clock::now();
-      approximate(state, fidelity, *dd);
-      const auto t2 = std::chrono::high_resolution_clock::now();
-      const std::chrono::duration<double, std::micro> runtime = t2 - t1;
+        const auto t1 = std::chrono::high_resolution_clock::now();
+        approximate(state, fidelity, *dd);
+        const auto t2 = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double, std::micro> runtime = t2 - t1;
 
-      std::cout << numNodes << ";" << runtime.count() << ";"
-                << runtime.count() /
-                       (static_cast<double>(numNodes) * std::log2(numNodes))
-                << '\n';
+        rt += runtime.count();
+      }
+
+      std::cout << nodes / repeats << " | " << rt / repeats << '\n';
     }
-    EXPECT_FALSE(true);
   }
 }
