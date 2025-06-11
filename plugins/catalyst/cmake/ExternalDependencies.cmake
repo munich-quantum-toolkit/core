35c01bfee12152d3fa1a85c83669265211c91271
flowerthrower <flowerthrower@users.noreply.github.com>
2025-06-11 13:54:46 +0200

    debug ci
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci

--- plugins/catalyst/cmake/ExternalDependencies.cmake
+++ plugins/catalyst/cmake/ExternalDependencies.cmake
@@ -8,55 +8,63 @@
 
 # Declare all external dependencies and make sure that they are available.
 
-# First try to find local Catalyst CMake installation
-set(CATALYST_VERSION 0.12.0)
-find_package(Catalyst ${CATALYST_VERSION} QUIET)
+if(DEFINED Python_EXECUTABLE AND Python_EXECUTABLE)
 
-if(Catalyst_FOUND)
-  message(STATUS "Found local Catalyst installation via CMake: ${Catalyst_DIR}")
-else()
-
-  if(DEFINED Python_EXECUTABLE AND Python_EXECUTABLE)
-
-    # Check if the pennylane-catalyst package is installed in the python environment.
-    execute_process(
-      COMMAND "${Python_EXECUTABLE}" -c "import catalyst; print(catalyst.__version__)"
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      OUTPUT_VARIABLE FOUND_CATALYST_VERSION)
-    if(FOUND_CATALYST_VERSION)
-      message(STATUS "Found pennylane-catalyst ${CATALYST_VERSION} in python environment.")
-      # Check if the version is compatible.
-      if(FOUND_CATALYST_VERSION VERSION_LESS ${CATALYST_VERSION})
-        message(
-          WARNING
-            "pennylane-catalyst version ${FOUND_CATALYST_VERSION} in python environment is not compatible."
-        )
-      else()
-        # Detect the installed catalyst include files.
-        execute_process(
-          COMMAND "${Python_EXECUTABLE}" -c
-                  "import catalyst.utils.runtime_environment as c; print(c.get_include_path())"
-          OUTPUT_STRIP_TRAILING_WHITESPACE
-          OUTPUT_VARIABLE CATALYST_INCLUDE_DIRS)
-      endif()
-    else()
-      # Unfortunately, the download for an individual package cannot be turned off. To avoid
-      # downloading the entire package, we use `find_package` instead.
-      find_package(Catalyst ${CATALYST_VERSION} REQUIRED)
-    endif()
-
-    if(NOT CATALYST_INCLUDE_DIRS)
+  set(CATALYST_VERSION 0.12.0)
+  # Check if the pennylane-catalyst package is installed in the python environment.
+  execute_process(
+    COMMAND "${Python_EXECUTABLE}" -c "import catalyst; print(catalyst.__version__)"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE FOUND_CATALYST_VERSION)
+  if(FOUND_CATALYST_VERSION)
+    message(STATUS "Found pennylane-catalyst ${CATALYST_VERSION} in python environment.")
+    # Check if the version is compatible.
+    if(FOUND_CATALYST_VERSION VERSION_LESS ${CATALYST_VERSION})
       message(
-        FATAL_ERROR
-          "The include directory of the pennylane-catalyst package could not be retrieved. Please ensure that the catalyst is installed correctly."
+        WARNING
+          "pennylane-catalyst version ${FOUND_CATALYST_VERSION} in python environment is not compatible."
       )
-    endif()
+    else()
+      # Detect the installed catalyst include files.
+      execute_process(
+        COMMAND "${Python_EXECUTABLE}" -c
+                "import catalyst.utils.runtime_environment as c; print(c.get_include_path())"
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        OUTPUT_VARIABLE CATALYST_INCLUDE_DIRS)
 
+      message(STATUS "Catalyst include path resolved to: ${CATALYST_INCLUDE_DIRS}")
+
+      string(FIND "${CATALYST_INCLUDE_DIRS}" "site-packages" SITEPKG_IDX)
+
+      if(SITEPKG_IDX EQUAL -1)
+        # In case of a installation from source Assume include path looks like: <root>/mlir/include
+        # Derive <root>/mlir/build/include and <root>/mlir/build/lib/cmake/catalyst
+        get_filename_component(CATALYST_MLIR_ROOT "${CATALYST_INCLUDE_DIRS}/.." ABSOLUTE)
+        set(CATALYST_BUILD_DIR "${CATALYST_MLIR_ROOT}/build")
+        set(CATALYST_BUILD_INCLUDE_DIR "${CATALYST_BUILD_DIR}/include")
+        set(Catalyst_DIR "${CATALYST_BUILD_DIR}/lib/cmake/catalyst")
+
+        include_directories("${CATALYST_INCLUDE_DIRS}")
+        include_directories("${CATALYST_BUILD_INCLUDE_DIR}")
+      endif()
+
+    endif()
   else()
+    # Unfortunately, the download for an individual package cannot be turned off. To avoid
+    # downloading the entire package, we use `find_package` instead.
+    find_package(Catalyst ${CATALYST_VERSION} REQUIRED)
+  endif()
+
+  if(NOT CATALYST_INCLUDE_DIRS)
     message(
       FATAL_ERROR
-        "Python executable is not defined. Please set the Python_EXECUTABLE variable to the path of the Python interpreter."
+        "The include directory of the pennylane-catalyst package could not be retrieved. Please ensure that the catalyst is installed correctly."
     )
   endif()
 
+else()
+  message(
+    FATAL_ERROR
+      "Python executable is not defined. Please set the Python_EXECUTABLE variable to the path of the Python interpreter."
+  )
 endif()
