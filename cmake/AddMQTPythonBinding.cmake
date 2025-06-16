6e9689747f31639826d7aabec06f5d8317b3b1dd
Daniel Haag <denialhaag@gmail.com>
2025-06-16 10:42:56 +0200

    Make CMake function for adding Python bindings reusable

--- cmake/AddMQTPythonBinding.cmake
+++ cmake/AddMQTPythonBinding.cmake
@@ -6,15 +6,13 @@
 #
 # Licensed under the MIT License
 
-function(add_mqt_core_binding target_name)
+function(add_mqt_python_binding package_name target_name)
   # parse the arguments
   cmake_parse_arguments(ARG "" "MODULE_NAME;INSTALL_DIR" "LINK_LIBS" ${ARGN})
   set(SOURCES ${ARG_UNPARSED_ARGUMENTS})
 
-  # set default "." for INSTALL_DIR
-  if(NOT ARG_INSTALL_DIR)
-    set(ARG_INSTALL_DIR ".")
-  endif()
+  set(MQT_PACKAGE_MODULE_NAME MQT_${package_name}_MODULE_NAME)
+  set(MQT_PACKAGE_TARGET_NAME MQT_${package_name}_TARGET_NAME)
 
   # declare the Python module
   pybind11_add_module(
@@ -27,13 +25,18 @@ function(add_mqt_core_binding target_name)
     # source code goes here
     ${SOURCES})
 
+  # set default "." for INSTALL_DIR
+  if(NOT ARG_INSTALL_DIR)
+    set(ARG_INSTALL_DIR ".")
+  endif()
+
   if(ARG_MODULE_NAME)
     # the library name must be the same as the module name
     set_target_properties(${target_name} PROPERTIES OUTPUT_NAME ${ARG_MODULE_NAME})
-    target_compile_definitions(${target_name} PRIVATE MQT_CORE_MODULE_NAME=${ARG_MODULE_NAME})
+    target_compile_definitions(${target_name} PRIVATE MQT_PACKAGE_MODULE_NAME=${ARG_MODULE_NAME})
   else()
     # use the target name as the module name
-    target_compile_definitions(${target_name} PRIVATE MQT_CORE_MODULE_NAME=${target_name})
+    target_compile_definitions(${target_name} PRIVATE MQT_PACKAGE_MODULE_NAME=${target_name})
   endif()
 
   # add project libraries to the link libraries
@@ -46,5 +49,5 @@ function(add_mqt_core_binding target_name)
   install(
     TARGETS ${target_name}
     DESTINATION ${ARG_INSTALL_DIR}
-    COMPONENT ${MQT_CORE_TARGET_NAME}_Python)
+    COMPONENT ${MQT_PACKAGE_TARGET_NAME}_Python)
 endfunction()
