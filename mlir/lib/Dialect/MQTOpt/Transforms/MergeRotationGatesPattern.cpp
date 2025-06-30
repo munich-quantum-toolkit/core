fb8465b543b1e68a19b1e2cef2f82f35d1f3997a
Daniel Haag <denialhaag@gmail.com>
2025-06-30 15:28:59 +0200

    Only support strict merging of simple rotation gates for now

--- mlir/lib/Dialect/MQTOpt/Transforms/MergeRotationGatesPattern.cpp
+++ mlir/lib/Dialect/MQTOpt/Transforms/MergeRotationGatesPattern.cpp
@@ -29,9 +29,6 @@
 
 namespace mqt::ir::opt {
 
-static constexpr auto PI = static_cast<double>(
-    3.141592653589793238462643383279502884197169399375105820974L);
-
 /**
  * @brief This pattern attempts to merge consecutive rotation gates.
  */
@@ -51,102 +48,13 @@ struct MergeRotationGatesPattern final
   [[nodiscard]] static bool areGatesMergeable(mlir::Operation& a,
                                               mlir::Operation& b) {
 
+    static const std::unordered_set<std::string> MERGEABLE_GATES = {
+        "gphase", "rx", "ry", "rz", "rxx", "ryy", "rzz", "rzx"};
+
     const auto aName = a.getName().stripDialect().str();
     const auto bName = b.getName().stripDialect().str();
 
-    if (aName != bName) {
-      return false;
-    }
-
-    static const std::unordered_set<std::string> MERGEABLE_GATES = {
-        "gphase", "rx", "ry", "rz", "rxx", "ryy", "rzz", "rzx"};
-    if (MERGEABLE_GATES.count(aName) == 1) {
-      return true;
-    }
-
-    if (aName == "xxminusyy" || aName == "xxplusyy") {
-      return areGatesMergeableXxMinusPlusYy(a, b);
-    }
-
-    if (aName == "u") {
-      return areGatesMergeableU(a, b);
-    }
-
-    if (aName == "u2") {
-      return areGatesMergeableU2(a, b);
-    }
-
-    return false;
-  }
-
-  /**
-   * @brief Checks if two consecutive xxminusyy/xxplusyy gates can be merged.
-   *
-   * Currently, this function only checks if the gates can be removed.
-   *
-   * @param a The first gate.
-   * @param b The second gate.
-   * @return True if the gates can be merged, false otherwise.
-   */
-  [[nodiscard]] static bool areGatesMergeableXxMinusPlusYy(mlir::Operation& a,
-                                                           mlir::Operation& b) {
-    auto unitaryA = mlir::dyn_cast<UnitaryInterface>(a);
-    auto unitaryB = mlir::dyn_cast<UnitaryInterface>(b);
-
-    auto aThetaDouble = getDoubleFromValue(unitaryA.getParams()[0]);
-    auto bThetaDouble = getDoubleFromValue(unitaryB.getParams()[0]);
-
-    return (aThetaDouble == -bThetaDouble);
-  }
-
-  /**
-   * @brief Checks if two consecutive u gates can be merged.
-   *
-   * Currently, this function only checks if the gates can be removed.
-   *
-   * @param a The first gate.
-   * @param b The second gate.
-   * @return True if the gates can be merged, false otherwise.
-   */
-  [[nodiscard]] static bool areGatesMergeableU(mlir::Operation& a,
-                                               mlir::Operation& b) {
-    auto unitaryA = mlir::dyn_cast<UnitaryInterface>(a);
-    auto unitaryB = mlir::dyn_cast<UnitaryInterface>(b);
-
-    auto aThetaDouble = getDoubleFromValue(unitaryA.getParams()[0]);
-    auto aPhiDouble = getDoubleFromValue(unitaryA.getParams()[1]);
-    auto aLambdaDouble = getDoubleFromValue(unitaryA.getParams()[2]);
-
-    auto bThetaDouble = getDoubleFromValue(unitaryB.getParams()[0]);
-    auto bPhiDouble = getDoubleFromValue(unitaryB.getParams()[1]);
-    auto bLambdaDouble = getDoubleFromValue(unitaryB.getParams()[2]);
-
-    return ((aThetaDouble == -bThetaDouble) && (aPhiDouble == -bLambdaDouble) &&
-            (aLambdaDouble == -bPhiDouble));
-  }
-
-  /**
-   * @brief Checks if two consecutive u2 gates can be merged.
-   *
-   * Currently, this function only checks if the gates can be removed.
-   *
-   * @param a The first gate.
-   * @param b The second gate.
-   * @return True if the gates can be merged, false otherwise.
-   */
-  [[nodiscard]] static bool areGatesMergeableU2(mlir::Operation& a,
-                                                mlir::Operation& b) {
-    auto unitaryA = mlir::dyn_cast<UnitaryInterface>(a);
-    auto unitaryB = mlir::dyn_cast<UnitaryInterface>(b);
-
-    auto aPhiDouble = getDoubleFromValue(unitaryA.getParams()[0]);
-    auto aLambdaDouble = getDoubleFromValue(unitaryA.getParams()[1]);
-
-    auto bPhiDouble = getDoubleFromValue(unitaryB.getParams()[0]);
-    auto bLambdaDouble = getDoubleFromValue(unitaryB.getParams()[1]);
-
-    return ((aPhiDouble == -bLambdaDouble - PI) &&
-            (aLambdaDouble == -bPhiDouble + PI));
+    return ((aName == bName) && (MERGEABLE_GATES.count(aName) == 1));
   }
 
   /**
@@ -186,96 +94,27 @@ struct MergeRotationGatesPattern final
   }
 
   /**
-   * @brief Converts an MLIR value to a double.
-   *
-   * @param value The MLIR value.
-   * @return The double.
-   */
-  static double getDoubleFromValue(mlir::Value value) {
-    auto constOp = value.getDefiningOp<mlir::arith::ConstantOp>();
-    auto floatAttr = mlir::dyn_cast<mlir::FloatAttr>(constOp.getValue());
-    return floatAttr.getValueAsDouble();
-  }
-
-  /**
-   * @brief Creates an MLIR value from a double.
-   *
-   * @param value The double value.
-   * @param rewriter The pattern rewriter.
-   * @param loc The location for the new value.
-   * @return The MLIR value.
-   */
-  static mlir::Value getValueFromDouble(double value,
-                                        mlir::PatternRewriter& rewriter,
-                                        mlir::Location loc) {
-    const auto f64Type = rewriter.getF64Type();
-    auto floatAttr = rewriter.getF64FloatAttr(value);
-    return rewriter.create<mlir::arith::ConstantOp>(loc, f64Type, floatAttr);
-  }
-
-  /**
-   * @brief Cancels two consecutive gates.
-   *
-   * @param op The operation to cancel.
-   * @param user The user of the operation to cancel.
-   * @param rewriter The pattern rewriter.
-   */
-  void static cancelGates(UnitaryInterface op, UnitaryInterface user,
-                          mlir::PatternRewriter& rewriter) {
-    // Prepare erasures of op and user
-    const auto& userUsers = user->getUsers();
-    const auto& opAllInQubits = op.getAllInQubits();
-    const auto& userAllOutQubits = user.getAllOutQubits();
-    for (const auto& userUser : userUsers) {
-      for (size_t i = 0; i < userUser->getOperands().size(); i++) {
-        const auto& operand = userUser->getOperand(i);
-        const auto found = std::find(userAllOutQubits.begin(),
-                                     userAllOutQubits.end(), operand);
-        if (found == userAllOutQubits.end()) {
-          continue;
-        }
-        const auto idx = std::distance(userAllOutQubits.begin(), found);
-        rewriter.modifyOpInPlace(
-            userUser, [&] { userUser->setOperand(i, opAllInQubits[idx]); });
-      }
-    }
-
-    // Erase user
-    rewriter.eraseOp(user);
-
-    // Erase op
-    rewriter.eraseOp(op);
-  }
-
-  /**
    * @brief Merges two consecutive rotation gates into a single gate.
    *
-   * The function supports gphase, rx, ry, rz, rxx, ryy, and rzz.
+   * The function supports gphase, rx, ry, rz, rxx, ryy, rzz, and rzx.
    * The gates are merged by adding their angles.
-   * If the angels add up to zero, the gates are removed altogether.
+   * The merged gate is not removed if the angles add up to zero.
    *
    * @param op The first instance of the rotation gate.
    * @param rewriter The pattern rewriter.
    */
-  void static rewriteSimpleRotation(UnitaryInterface op,
-                                    mlir::PatternRewriter& rewriter) {
+  void static rewriteAdditiveAngle(UnitaryInterface op,
+                                   mlir::PatternRewriter& rewriter) {
     auto const type = op->getName().stripDialect().str();
 
     auto user = mlir::dyn_cast<UnitaryInterface>(*op->getUsers().begin());
-
-    auto opParamDouble = getDoubleFromValue(op.getParams()[0]);
-    auto userParamDouble = getDoubleFromValue(user.getParams()[0]);
-
-    double const newParamValue = opParamDouble + userParamDouble;
-
-    if (newParamValue == 0.0) {
-      cancelGates(op, user, rewriter);
-      return;
-    }
-
     auto loc = user->getLoc();
-    auto newParam = getValueFromDouble(newParamValue, rewriter, loc);
-    const llvm::SmallVector<mlir::Value, 1> newParamsVec{newParam};
+
+    auto opParam = op.getParams()[0];
+    auto userParam = user.getParams()[0];
+
+    auto add = rewriter.create<mlir::arith::AddFOp>(loc, opParam, userParam);
+    const llvm::SmallVector<mlir::Value, 1> newParamsVec{add.getResult()};
     const mlir::ValueRange newParams(newParamsVec);
 
     auto userInQubits = user.getInQubits();
@@ -357,53 +196,13 @@ struct MergeRotationGatesPattern final
     rewriter.eraseOp(op);
   }
 
-  /**
-   * @brief Removes two consecutive xxminusyy/xxplusyy gates.
-   *
-   * @param op The first instance of the gate.
-   * @param rewriter The pattern rewriter.
-   */
-  void static rewriteXxMinusPlusYy(UnitaryInterface op,
-                                   mlir::PatternRewriter& rewriter) {
-    auto user = mlir::dyn_cast<UnitaryInterface>(*op->getUsers().begin());
-    cancelGates(op, user, rewriter);
-  }
-
-  /**
-   * @brief Removes two consecutive u gates.
-   *
-   * @param op The first instance of the gate.
-   * @param rewriter The pattern rewriter.
-   */
-  void static rewriteU(UnitaryInterface op, mlir::PatternRewriter& rewriter) {
-    auto user = mlir::dyn_cast<UnitaryInterface>(*op->getUsers().begin());
-    cancelGates(op, user, rewriter);
-  }
-
-  /**
-   * @brief Removes two consecutive u2 gates.
-   *
-   * @param op The first instance of the gate.
-   * @param rewriter The pattern rewriter.
-   */
-  void static rewriteU2(UnitaryInterface op, mlir::PatternRewriter& rewriter) {
-    auto user = mlir::dyn_cast<UnitaryInterface>(*op->getUsers().begin());
-    cancelGates(op, user, rewriter);
-  }
-
   void rewrite(UnitaryInterface op,
                mlir::PatternRewriter& rewriter) const override {
     auto const type = op->getName().stripDialect().str();
 
     if (type == "gphase" || type == "rx" || type == "ry" || type == "rz" ||
         type == "rxx" || type == "ryy" || type == "rzz" || type == "rzx") {
-      rewriteSimpleRotation(op, rewriter);
-    } else if (type == "xxminusyy" || type == "xxplusyy") {
-      rewriteXxMinusPlusYy(op, rewriter);
-    } else if (type == "u") {
-      rewriteU(op, rewriter);
-    } else if (type == "u2") {
-      rewriteU2(op, rewriter);
+      rewriteAdditiveAngle(op, rewriter);
     } else {
       throw std::runtime_error("Unsupported operation type: " + type);
     }
--- mlir/test/Dialect/MQTOpt/Transforms/merge-rotation-gates.mlir
+++ mlir/test/Dialect/MQTOpt/Transforms/merge-rotation-gates.mlir
@@ -35,20 +35,17 @@ module {
     // CHECK-NOT: %[[ANY:.*]] = mqtopt.rx(%[[ANY:.*]]) %[[ANY:.*]] : !mqtopt.Qubit
 
     %c_0 = arith.constant 1.000000e+00 : f64
-    %c_1 = arith.constant -1.000000e+00 : f64
     %q0_1 = mqtopt.rx(%c_0) %q0_0 : !mqtopt.Qubit
     %q0_2 = mqtopt.rx(%c_0) %q0_1 : !mqtopt.Qubit
     %q0_3 = mqtopt.rx(%c_0) %q0_2 : !mqtopt.Qubit
     %q1_1 = mqtopt.ry(%c_0) %q1_0 : !mqtopt.Qubit
     %q1_2 = mqtopt.rz(%c_0) %q1_1 : !mqtopt.Qubit
     %q1_3 = mqtopt.rz(%c_0) %q1_2 : !mqtopt.Qubit
-    %q1_4 = mqtopt.ry(%c_0) %q1_3 : !mqtopt.Qubit
-    %q1_5 = mqtopt.ry(%c_1) %q1_4 : !mqtopt.Qubit
 
     // CHECK: %[[Reg_3:.*]] = "mqtopt.insertQubit"(%[[Reg_2]], %[[Q0_3]])  <{index_attr = 0 : i64}>
     %reg_3 = "mqtopt.insertQubit"(%reg_2, %q0_3) <{index_attr = 0 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
     // CHECK: %[[Reg_4:.*]] = "mqtopt.insertQubit"(%[[Reg_3]], %[[Q1_3]])  <{index_attr = 1 : i64}>
-    %reg_4 = "mqtopt.insertQubit"(%reg_3, %q1_5) <{index_attr = 1 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
+    %reg_4 = "mqtopt.insertQubit"(%reg_3, %q1_3) <{index_attr = 1 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
 
     // CHECK: "mqtopt.deallocQubitRegister"(%[[Reg_4]])
     "mqtopt.deallocQubitRegister"(%reg_4) : (!mqtopt.QubitRegister) -> ()
@@ -87,117 +84,21 @@ module {
     // CHECK-NOT: %[[ANY:.*]]:2 = mqtopt.rzz(%[[ANY:.*]]) %[[ANY:.*]], %[[ANY:.*]] : !mqtopt.Qubit, !mqtopt.Qubit
 
     %c_0 = arith.constant 1.000000e+00 : f64
-    %c_1 = arith.constant -1.000000e+00 : f64
     %q01_1:2 = mqtopt.rxx(%c_0) %q0_0, %q1_0 : !mqtopt.Qubit, !mqtopt.Qubit
     %q12_1:2 = mqtopt.rxx(%c_0) %q01_1#1, %q2_0 : !mqtopt.Qubit, !mqtopt.Qubit
     %q12_2:2 = mqtopt.rxx(%c_0) %q12_1#0, %q12_1#1 : !mqtopt.Qubit, !mqtopt.Qubit
     %q01_2:2 = mqtopt.ryy(%c_0) %q01_1#0, %q12_2#0 : !mqtopt.Qubit, !mqtopt.Qubit
     %q01_3:2 = mqtopt.ryy(%c_0) %q01_2#0, %q01_2#1 : !mqtopt.Qubit, !mqtopt.Qubit
-    %q12_3:2 = mqtopt.rzz(%c_0) %q01_3#1, %q12_2#1 : !mqtopt.Qubit, !mqtopt.Qubit
-    %q12_4:2 = mqtopt.rzz(%c_1) %q12_3#0, %q12_3#1 : !mqtopt.Qubit, !mqtopt.Qubit
 
     // CHECK: %[[Reg_4:.*]] = "mqtopt.insertQubit"(%[[Reg_3]], %[[Q01_3]]#0)  <{index_attr = 0 : i64}>
     %reg_4 = "mqtopt.insertQubit"(%reg_3, %q01_3#0) <{index_attr = 0 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
     // CHECK: %[[Reg_5:.*]] = "mqtopt.insertQubit"(%[[Reg_4]], %[[Q01_3]]#1)  <{index_attr = 1 : i64}>
-    %reg_5 = "mqtopt.insertQubit"(%reg_4, %q12_4#0) <{index_attr = 1 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
+    %reg_5 = "mqtopt.insertQubit"(%reg_4, %q01_3#1) <{index_attr = 1 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
     // CHECK: %[[Reg_6:.*]] = "mqtopt.insertQubit"(%[[Reg_5]], %[[Q12_2]]#1)  <{index_attr = 2 : i64}>
-    %reg_6 = "mqtopt.insertQubit"(%reg_5, %q12_4#1) <{index_attr = 2 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
-
-    // CHECK: "mqtopt.deallocQubitRegister"(%[[Reg_6]])
-    "mqtopt.deallocQubitRegister"(%reg_6) : (!mqtopt.QubitRegister) -> ()
-
-    return
-  }
-}
-
-// -----
-// This test checks if consecutive xxminusyy and xxplusyy gates are canceled correctly.
-// If a gate has no consecutive partner, it is left as is.
-// If consecutive gates A and B satisfy theta_A == -theta_B, they are canceled.
-// Currently, we do not support the merging of xxminusyy and xxplusyy gates.
-
-module {
-  // CHECK-LABEL: func.func @testCancelXxMinusPlusYyGates
-  func.func @testCancelXxMinusPlusYyGates() {
-    // CHECK: %[[Res_2:.*]] = arith.constant 2.000000e+00 : f64
-    // CHECK: %[[Res_1:.*]] = arith.constant 1.000000e+00 : f64
-
-    // CHECK: %[[Reg_0:.*]] = "mqtopt.allocQubitRegister"
-    %reg_0 = "mqtopt.allocQubitRegister"() <{size_attr = 3 : i64}> : () -> !mqtopt.QubitRegister
-
-    // CHECK: %[[Reg_1:.*]], %[[Q0_0:.*]] = "mqtopt.extractQubit"(%[[Reg_0]]) <{index_attr = 0 : i64}>
-    %reg_1, %q0_0 = "mqtopt.extractQubit"(%reg_0) <{index_attr = 0 : i64}> : (!mqtopt.QubitRegister) -> (!mqtopt.QubitRegister, !mqtopt.Qubit)
-    // CHECK: %[[Reg_2:.*]], %[[Q1_0:.*]] = "mqtopt.extractQubit"(%[[Reg_1]]) <{index_attr = 1 : i64}>
-    %reg_2, %q1_0 = "mqtopt.extractQubit"(%reg_1) <{index_attr = 1 : i64}> : (!mqtopt.QubitRegister) -> (!mqtopt.QubitRegister, !mqtopt.Qubit)
-    // CHECK: %[[Reg_3:.*]], %[[Q2_0:.*]] = "mqtopt.extractQubit"(%[[Reg_2]]) <{index_attr = 2 : i64}>
-    %reg_3, %q2_0 = "mqtopt.extractQubit"(%reg_2) <{index_attr = 2 : i64}> : (!mqtopt.QubitRegister) -> (!mqtopt.QubitRegister, !mqtopt.Qubit)
-
-    // CHECK: %[[Q01_1:.*]]:2 = mqtopt.xxplusyy(%[[Res_1:.*]], %[[Res_2:.*]]) %[[Q0_0]], %[[Q1_0]] : !mqtopt.Qubit, !mqtopt.Qubit
-    // CHECK-NOT: %[[ANY:.*]]:2 = mqtopt.xxminusyy(%[[ANY:.*]], %[[ANY:.*]]), %[[ANY:.*]] : !mqtopt.Qubit, !mqtopt.Qubit
-
-    %c_0 = arith.constant -1.000000e+00 : f64
-    %c_1 = arith.constant 1.000000e+00 : f64
-    %c_2 = arith.constant 2.000000e+00 : f64
-    %q01_1:2 = mqtopt.xxplusyy(%c_1, %c_2) %q0_0, %q1_0 : !mqtopt.Qubit, !mqtopt.Qubit
-    %q12_1:2 = mqtopt.xxminusyy(%c_1, %c_2) %q01_1#1, %q2_0 : !mqtopt.Qubit, !mqtopt.Qubit
-    %q12_2:2 = mqtopt.xxminusyy(%c_0, %c_2) %q12_1#0, %q12_1#1 : !mqtopt.Qubit, !mqtopt.Qubit
-
-    // CHECK: %[[Reg_4:.*]] = "mqtopt.insertQubit"(%[[Reg_3]], %[[Q01_1]]#0)  <{index_attr = 0 : i64}>
-    %reg_4 = "mqtopt.insertQubit"(%reg_3, %q01_1#0) <{index_attr = 0 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
-    // CHECK: %[[Reg_5:.*]] = "mqtopt.insertQubit"(%[[Reg_4]], %[[ANY:.*]])  <{index_attr = 1 : i64}>
-    %reg_5 = "mqtopt.insertQubit"(%reg_4, %q12_2#0) <{index_attr = 1 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
-    // CHECK: %[[Reg_6:.*]] = "mqtopt.insertQubit"(%[[Reg_5]], %[[ANY:.*]])  <{index_attr = 2 : i64}>
     %reg_6 = "mqtopt.insertQubit"(%reg_5, %q12_2#1) <{index_attr = 2 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
 
     // CHECK: "mqtopt.deallocQubitRegister"(%[[Reg_6]])
     "mqtopt.deallocQubitRegister"(%reg_6) : (!mqtopt.QubitRegister) -> ()
-
-    return
-  }
-}
-
-// -----
-// This test checks if consecutive u and u2 gates are canceled correctly.
-// If a gate has no consecutive partner, it is left as is.
-// If consecutive gates A and B have compatible parameters, they are canceled.
-// Currently, we do not support the merging of u and u2 gates.
-
-module {
-  // CHECK-LABEL: func.func @testCancelUGates
-  func.func @testCancelUGates() {
-    // CHECK: %[[Res_p2:.*]] = arith.constant 2.000000e+00 : f64
-    // CHECK: %[[Res_p1:.*]] = arith.constant 1.000000e+00 : f64
-    // CHECK: %[[Res_m1:.*]] = arith.constant -1.000000e+00 : f64
-
-    // CHECK: %[[Reg_0:.*]] = "mqtopt.allocQubitRegister"
-    %reg_0 = "mqtopt.allocQubitRegister"() <{size_attr = 2 : i64}> : () -> !mqtopt.QubitRegister
-
-    // CHECK: %[[Reg_1:.*]], %[[Q0_0:.*]] = "mqtopt.extractQubit"(%[[Reg_0]]) <{index_attr = 0 : i64}>
-    %reg_1, %q0_0 = "mqtopt.extractQubit"(%reg_0) <{index_attr = 0 : i64}> : (!mqtopt.QubitRegister) -> (!mqtopt.QubitRegister, !mqtopt.Qubit)
-    // CHECK: %[[Reg_2:.*]], %[[Q1_0:.*]] = "mqtopt.extractQubit"(%[[Reg_1]]) <{index_attr = 1 : i64}>
-    %reg_2, %q1_0 = "mqtopt.extractQubit"(%reg_1) <{index_attr = 1 : i64}> : (!mqtopt.QubitRegister) -> (!mqtopt.QubitRegister, !mqtopt.Qubit)
-
-    // CHECK: %[[Q0_1:.*]] = mqtopt.u(%[[Res_m1:.*]], %[[Res_p1:.*]], %[[Res_p1:.*]]) %[[Q0_0]] : !mqtopt.Qubit
-    // CHECK: %[[Q0_2:.*]] = mqtopt.u(%[[Res_m1:.*]], %[[Res_m1:.*]], %[[Res_m1:.*]]) %[[Q0_1]] : !mqtopt.Qubit
-    // CHECK: %[[Q0_3:.*]] = mqtopt.u2(%[[Res_p1:.*]], %[[Res_p2:.*]]) %[[Q0_2]] : !mqtopt.Qubit
-    // CHECK-NOT: %[[ANY:.*]] = mqtopt.u(%[[ANY:.*]], %[[ANY:.*]], %[[ANY:.*]]) %[[ANY:.*]] : !mqtopt.Qubit
-
-    %c_0 = arith.constant -1.000000e+00 : f64
-    %c_1 = arith.constant 1.000000e+00 : f64
-    %c_2 = arith.constant 2.000000e+00 : f64
-    %q0_1 = mqtopt.u(%c_0, %c_1, %c_1) %q0_0 : !mqtopt.Qubit
-    %q0_2 = mqtopt.u(%c_0, %c_0, %c_0) %q0_1 : !mqtopt.Qubit
-    %q0_3 = mqtopt.u2(%c_1, %c_2) %q0_2 : !mqtopt.Qubit
-    %q1_1 = mqtopt.u(%c_0, %c_1, %c_1) %q1_0 : !mqtopt.Qubit
-    %q1_2 = mqtopt.u(%c_1, %c_0, %c_0) %q1_1 : !mqtopt.Qubit
-
-    // CHECK: %[[Reg_3:.*]] = "mqtopt.insertQubit"(%[[Reg_2]], %[[Q0_3]])  <{index_attr = 0 : i64}>
-    %reg_3 = "mqtopt.insertQubit"(%reg_2, %q0_3) <{index_attr = 0 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
-    // CHECK: %[[Reg_4:.*]] = "mqtopt.insertQubit"(%[[Reg_3]], %[[ANY:.*]])  <{index_attr = 1 : i64}>
-    %reg_4 = "mqtopt.insertQubit"(%reg_3, %q1_2) <{index_attr = 1 : i64}> : (!mqtopt.QubitRegister, !mqtopt.Qubit) -> !mqtopt.QubitRegister
-
-    // CHECK: "mqtopt.deallocQubitRegister"(%[[Reg_4]])
-    "mqtopt.deallocQubitRegister"(%reg_4) : (!mqtopt.QubitRegister) -> ()
 
     return
   }
