94f4096f8dba3883f0083602b9563931afb712aa
Daniel Haag <denialhaag@gmail.com>
2025-07-23 14:12:50 +0200

    Do not construct object before emplacing

--- mlir/lib/Conversion/MQTDynToMQTOpt/MQTDynToMQTOpt.cpp
+++ mlir/lib/Conversion/MQTDynToMQTOpt/MQTDynToMQTOpt.cpp
@@ -215,10 +215,8 @@ struct ConvertMQTDynExtract final : OpConversionPattern<dyn::ExtractOp> {
 
     // add an entry to the qubitDataMap to store the indices and the register
     // for the insertOperation
-    qubitDataMap->try_emplace(dynQubit,
-                              QubitData{.qReg = dynQreg,
-                                        .index = op.getIndex(),
-                                        .indexAttr = op.getIndexAttrAttr()});
+    qubitDataMap->try_emplace(dynQubit, dynQreg, op.getIndex(),
+                              op.getIndexAttrAttr());
 
     // append the entry to the qregQubitsMap to store which qubits that were
     // extracted from the register
