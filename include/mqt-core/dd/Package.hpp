munich-quantum-toolkit/core

commit eb1cbfc4177b52a9166f5ea9ac695088252e3267

Use contains to check for membership 2.0

include/mqt-core/dd/Package.hpp
@@ -1821,7 +1821,7 @@ public:
           if (edge.w.approximatelyZero()) {
             continue;
           }
-          if (mappedNode.find(edge.p) != mappedNode.end()) {
+          if (mappedNode.contains(edge.p)) {
             continue;
           }
 
@@ -1840,7 +1840,7 @@ public:
         if (edge.w.approximatelyZero()) {
           continue;
         }
-        if (mappedNode.find(edge.p) != mappedNode.end()) {
+        if (mappedNode.contains(edge.p)) {
           continue;
         }
         hasChild = edge.p == stack.top()->p;
@@ -1852,7 +1852,7 @@ public:
         stack.push(currentEdge);
         currentEdge = temp;
       } else {
-        if (mappedNode.find(currentEdge->p) != mappedNode.end()) {
+        if (!mappedNode.contains(currentEdge->p)) {
           currentEdge = nullptr;
           continue;
         }

include/mqt-core/datastructures/DirectedAcyclicGraph.hpp
@@ -38,10 +38,10 @@ public:
     closureMatrix[i][i] = true;
   }
   auto addEdge(const V& u, const V& v) -> void override {
-    if (this->mapping.find(u) == this->mapping.end()) {
+    if (!this->mapping.contains(u)) {
       addVertex(u);
     }
-    if (this->mapping.find(v) == this->mapping.end()) {
+    if (!this->mapping.contains(v)) {
       addVertex(v);
     }
     std::size_t const i = this->mapping.at(u);
@@ -62,10 +62,10 @@ public:
     }
   }
   [[nodiscard]] auto isReachable(const V& u, const V& v) const -> bool {
-    if (this->mapping.find(u) == this->mapping.end()) {
+    if (!this->mapping.contains(u)) {
       throw std::invalid_argument("Vertex u not in graph.");
     }
-    if (this->mapping.find(v) == this->mapping.end()) {
+    if (!this->mapping.contains(v)) {
       throw std::invalid_argument("Vertex v not in graph.");
     }
     return closureMatrix[this->mapping.at(u)][this->mapping.at(v)];

include/mqt-core/datastructures/DirectedGraph.hpp
@@ -59,7 +59,7 @@ public:
   virtual ~DirectedGraph() = default;
   virtual auto addVertex(const V& v) -> void {
     // check whether the vertex is already in the graph, if so do nothing
-    if (mapping.find(v) != mapping.end()) {
+    if (mapping.contains(v)) {
       std::stringstream ss;
       ss << "The vertex " << v << " is already in the graph.";
       throw std::invalid_argument(ss.str());
@@ -75,10 +75,10 @@ public:
     outDegrees.emplace_back(0);
   }
   virtual auto addEdge(const V& u, const V& v) -> void {
-    if (mapping.find(u) == mapping.end()) {
+    if (!mapping.contains(u)) {
       addVertex(u);
     }
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       addVertex(v);
     }
     const auto i = mapping.at(u);
@@ -93,7 +93,7 @@ public:
   [[nodiscard]] auto getNVertices() const -> std::size_t { return nVertices; }
   [[nodiscard]] auto getNEdges() const -> std::size_t { return nEdges; }
   [[nodiscard]] auto getInDegree(const V& v) const -> std::size_t {
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       std::stringstream ss;
       ss << "The vertex " << v << " is not in the graph.";
       throw std::invalid_argument(ss.str());
@@ -102,7 +102,7 @@ public:
     return inDegrees[i];
   }
   [[nodiscard]] auto getOutDegree(const V& v) const -> std::size_t {
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       std::stringstream ss;
       ss << "The vertex " << v << " is not in the graph.";
       throw std::invalid_argument(ss.str());
@@ -119,12 +119,12 @@ public:
                            });
   }
   [[nodiscard]] auto isEdge(const V& u, const V& v) const -> bool {
-    if (mapping.find(u) == mapping.end()) {
+    if (!mapping.contains(u)) {
       std::stringstream ss;
       ss << "The vertex " << u << " is not in the graph.";
       throw std::invalid_argument(ss.str());
     }
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       std::stringstream ss;
       ss << "The vertex " << v << " is not in the graph.";
       throw std::invalid_argument(ss.str());

include/mqt-core/datastructures/UndirectedGraph.hpp
@@ -62,7 +62,7 @@ protected:
 public:
   auto addVertex(const V& v) -> void {
     // check whether the vertex is already in the graph, if so do nothing
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       mapping[v] = nVertices;
       invMapping.emplace_back(v);
       ++nVertices;
@@ -80,10 +80,10 @@ public:
     }
   }
   auto addEdge(const V& u, const V& v, const E& e) -> void {
-    if (mapping.find(u) == mapping.end()) {
+    if (!mapping.contains(u)) {
       addVertex(u);
     }
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       addVertex(v);
     }
     const auto i = mapping.at(u);
@@ -132,7 +132,7 @@ public:
   }
   [[nodiscard]] auto getAdjacentEdges(const V& v) const
       -> std::unordered_set<std::pair<V, V>, PairHash<V, V>> {
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       std::stringstream ss;
       ss << "The vertex " << v << " is not in the graph.";
       throw std::invalid_argument(ss.str());
@@ -149,7 +149,7 @@ public:
     return result;
   }
   [[nodiscard]] auto getNeighbours(const V& v) const -> std::unordered_set<V> {
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       std::stringstream ss;
       ss << "The vertex " << v << " is not in the graph.";
       throw std::invalid_argument(ss.str());
@@ -165,7 +165,7 @@ public:
     return result;
   }
   [[nodiscard]] auto getDegree(const V& v) const -> std::size_t {
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       std::stringstream ss;
       ss << "The vertex " << v << " is not in the graph.";
       throw std::invalid_argument(ss.str());
@@ -182,12 +182,12 @@ public:
                            });
   }
   [[nodiscard]] auto isAdjacent(const V& u, const V& v) const -> bool {
-    if (mapping.find(u) == mapping.end()) {
+    if (!mapping.contains(u)) {
       std::stringstream ss;
       ss << "The vertex " << u << " is not in the graph.";
       throw std::invalid_argument(ss.str());
     }
-    if (mapping.find(v) == mapping.end()) {
+    if (!mapping.contains(v)) {
       std::stringstream ss;
       ss << "The vertex " << v << " is not in the graph.";
       throw std::invalid_argument(ss.str());

include/mqt-core/ir/operations/Expression.hpp
@@ -210,7 +210,7 @@ public:
    */
   [[nodiscard]] bool
   totalAssignment(const VariableAssignment& assignment) const {
-    return assignment.find(getVar()) != assignment.end();
+    return assignment.contains(getVar());
   }
 
   /**

src/dd/Edge.cpp
@@ -96,7 +96,7 @@ auto Edge<Node>::size(std::unordered_set<const Node*>& visited) const
   std::size_t sum = 1U;
   if (!isTerminal()) {
     for (const auto& e : p->e) {
-      if (visited.find(e.p) == visited.end()) {
+      if (!visited.contains(e.p)) {
         sum += e.size(visited);
       }
     }

src/ir/operations/StandardOperation.cpp
@@ -503,10 +503,10 @@ auto StandardOperation::commutesAtQubit(const Operation& other,
   if (!actsOn(qubit) || !other.actsOn(qubit)) {
     return true;
   }
-  if (controls.find(qubit) != controls.end()) {
+  if (controls.contains(qubit)) {
     // if this is controlled on the given qubit
     if (const auto& controls2 = other.getControls();
-        controls2.find(qubit) != controls2.end()) {
+        controls2.contains(qubit)) {
       // if other is controlled on the given qubit
       // q: ──■────■──
       //      |    |
@@ -520,8 +520,7 @@ auto StandardOperation::commutesAtQubit(const Operation& other,
     //      |  └────┘
   }
   // here: qubit is a target of this
-  if (const auto& controls2 = other.getControls();
-      controls2.find(qubit) != controls2.end()) {
+  if (const auto& controls2 = other.getControls(); controls2.contains(qubit)) {
     return isDiagonalGate();
     // true, iff qubit is a target and this is a diagonal gate and other is
     // controlled, e.g.

src/na/NAComputation.cpp
@@ -98,10 +98,10 @@ auto NAComputation::validate() const -> std::pair<bool, std::string> {
         //===-----------------------------------------------------------------//
         // Load Operations
         //-----------------------------------------------------------------===//
-        if (std::ranges::any_of(opAtoms, [&currentlyShuttling](
-                                             const auto* atom) {
-              return currentlyShuttling.find(atom) != currentlyShuttling.end();
-            })) {
+        if (std::ranges::any_of(opAtoms,
+                                [&currentlyShuttling](const auto* atom) {
+                                  return currentlyShuttling.contains(atom);
+                                })) {
           ss << "Error in op number " << counter << " (atom already loaded)\n";
           return {false, ss.str()};
         }
@@ -112,10 +112,10 @@ auto NAComputation::validate() const -> std::pair<bool, std::string> {
         //===-----------------------------------------------------------------//
         // Move and Store Operations
         //-----------------------------------------------------------------===//
-        if (std::ranges::any_of(opAtoms, [&currentlyShuttling](
-                                             const auto* atom) {
-              return currentlyShuttling.find(atom) == currentlyShuttling.end();
-            })) {
+        if (std::ranges::any_of(opAtoms,
+                                [&currentlyShuttling](const auto* atom) {
+                                  return !currentlyShuttling.contains(atom);
+                                })) {
           ss << "Error in op number " << counter << " (atom not loaded)\n";
           return {false, ss.str()};
         }

commit 855e7a2f9430cf4e2abda661f845286333c3c883

🐛 Fix bug in DD package (#1088)
## Description

This PR fixes a bug in the DD package that I introduced in
https://github.com/munich-quantum-toolkit/core/pull/897/commits/eb1cbfc4177b52a9166f5ea9ac695088252e3267.

## Checklist:

- [x] The pull request only contains commits that are focused and
relevant to this change.
- [ ] I have added appropriate tests that cover the new/changed
functionality.
- [x] ~~I have updated the documentation to reflect these changes.~~
- [ ] I have added entries to the changelog for any noteworthy
additions, changes, fixes, or removals.
- [x] ~~I have added migration instructions to the upgrade guide (if
needed).~~
- [x] The changes follow the project's style guidelines and introduce no
new warnings.
- [x] The changes are fully tested and pass the CI checks.
- [x] I have reviewed my own code changes.

include/mqt-core/dd/Package.hpp
@@ -1852,7 +1852,7 @@ public:
         stack.push(currentEdge);
         currentEdge = temp;
       } else {
-        if (!mappedNode.contains(currentEdge->p)) {
+        if (mappedNode.contains(currentEdge->p)) {
           currentEdge = nullptr;
           continue;
         }