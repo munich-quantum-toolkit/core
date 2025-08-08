41279b88910d17ea92204ecb724d598ab1b4ab1a
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-08-08 18:26:50 +0200

    Spit code by creating functions

--- mlir/lib/Dialect/MQTDyn/Translation/ImportQuantumComputation.cpp
+++ mlir/lib/Dialect/MQTDyn/Translation/ImportQuantumComputation.cpp
@@ -25,47 +25,40 @@
 #include <mlir/IR/ValueRange.h>
 #include <vector>
 
-mlir::OwningOpRef<mlir::ModuleOp>
-translateQuantumComputationToMLIR(mlir::MLIRContext& context,
-                                  qc::QuantumComputation& quantumComputation) {
-
-  mlir::OpBuilder builder(&context);
-
-  auto module = builder.create<mlir::ModuleOp>(builder.getUnknownLoc());
-  builder.setInsertionPointToStart(module.getBody());
-
-  // Create function
-  auto funcType = builder.getFunctionType({}, {});
-
-  auto mainFunc = builder.create<mlir::func::FuncOp>(builder.getUnknownLoc(),
-                                                     "main", funcType);
-
-  auto& entryBlock = mainFunc.getBody().emplaceBlock();
-  builder.setInsertionPointToStart(&entryBlock);
-
-  // Define register
+mlir::Value allocateQreg(mlir::OpBuilder& builder, mlir::MLIRContext& context,
+                         std::size_t numQubits) {
   const auto& qregType = mqt::ir::dyn::QubitRegisterType::get(&context);
-  auto numQubits = quantumComputation.getNqubits();
   auto sizeAttr = builder.getI64IntegerAttr(numQubits);
 
   auto allocOp = builder.create<mqt::ir::dyn::AllocOp>(
       builder.getUnknownLoc(), qregType, nullptr, sizeAttr);
 
-  // Extract qubits
-  const auto& qubitType = mqt::ir::dyn::QubitType::get(&context);
+  return allocOp.getResult();
+}
 
-  std::vector<mlir::Value> allQubits = {};
-  allQubits.reserve(numQubits);
+std::vector<mlir::Value> extractQubits(mlir::OpBuilder& builder,
+                                       mlir::MLIRContext& context,
+                                       mlir::Value quantumRegister,
+                                       std::size_t numQubits) {
+  const auto& qubitType = mqt::ir::dyn::QubitType::get(&context);
+  std::vector<mlir::Value> qubits;
+  qubits.reserve(numQubits);
 
   for (std::size_t qubit = 0; qubit < numQubits; ++qubit) {
     auto indexAttr = builder.getI64IntegerAttr(qubit);
     auto extractOp = builder.create<mqt::ir::dyn::ExtractOp>(
-        builder.getUnknownLoc(), qubitType, allocOp.getResult(), nullptr,
+        builder.getUnknownLoc(), qubitType, quantumRegister, nullptr,
         indexAttr);
-    allQubits.push_back(extractOp.getResult());
+    qubits.push_back(extractOp.getResult());
   }
 
-  // Add gates
+  return qubits;
+}
+
+void addOperations(mlir::OpBuilder& builder,
+                   const qc::QuantumComputation& quantumComputation,
+                   const std::vector<mlir::Value>& qubits) {
+
   for (const auto& operation : quantumComputation) {
     if (operation->getType() == qc::OpType::H) {
       const mlir::ValueRange params;
@@ -73,7 +66,7 @@ translateQuantumComputationToMLIR(mlir::MLIRContext& context,
       const mlir::ValueRange negCtrlQubits;
 
       auto target = operation->getTargets()[0];
-      const mlir::SmallVector<mlir::Value, 1> inQubitsVec = {allQubits[target]};
+      const mlir::SmallVector<mlir::Value, 1> inQubitsVec = {qubits[target]};
       const mlir::ValueRange inQubits = {inQubitsVec};
 
       builder.create<mqt::ir::dyn::HOp>(builder.getUnknownLoc(), nullptr,
@@ -85,7 +78,7 @@ translateQuantumComputationToMLIR(mlir::MLIRContext& context,
       const mlir::ValueRange negCtrlQubits;
 
       auto target = operation->getTargets()[0];
-      const mlir::SmallVector<mlir::Value, 1> inQubitsVec = {allQubits[target]};
+      const mlir::SmallVector<mlir::Value, 1> inQubitsVec = {qubits[target]};
       const mlir::ValueRange inQubits = {inQubitsVec};
 
       std::vector<mlir::Value> controlsVec;
@@ -93,7 +86,7 @@ translateQuantumComputationToMLIR(mlir::MLIRContext& context,
       if (!controls.empty()) {
         controlsVec.reserve(controls.size());
         for (const auto& control : controls) {
-          controlsVec.push_back(allQubits[control.qubit]);
+          controlsVec.push_back(qubits[control.qubit]);
         }
         posCtrlQubits = mlir::ValueRange{controlsVec};
       }
@@ -103,6 +96,34 @@ translateQuantumComputationToMLIR(mlir::MLIRContext& context,
                                         posCtrlQubits, negCtrlQubits);
     }
   }
+}
+
+mlir::OwningOpRef<mlir::ModuleOp>
+translateQuantumComputationToMLIR(mlir::MLIRContext& context,
+                                  qc::QuantumComputation& quantumComputation) {
+
+  mlir::OpBuilder builder(&context);
+
+  // Create module
+  auto module = builder.create<mlir::ModuleOp>(builder.getUnknownLoc());
+  builder.setInsertionPointToStart(module.getBody());
+
+  // Create main function as entry point
+  auto funcType = builder.getFunctionType({}, {});
+
+  auto mainFunc = builder.create<mlir::func::FuncOp>(builder.getUnknownLoc(),
+                                                     "main", funcType);
+
+  auto& entryBlock = mainFunc.getBody().emplaceBlock();
+  builder.setInsertionPointToStart(&entryBlock);
+
+  // Parse quantum computation
+  auto numQubits = quantumComputation.getNqubits();
+  auto qreg = allocateQreg(builder, context, numQubits);
+
+  auto qubits = extractQubits(builder, context, qreg, numQubits);
+
+  addOperations(builder, quantumComputation, qubits);
 
   // Create terminator
   builder.create<mlir::func::ReturnOp>(builder.getUnknownLoc());
