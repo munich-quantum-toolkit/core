6c04b8ce17a7d72e4bc8bf299d970d676f416a0e
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-08-13 15:07:14 +0200

    Link against Quake

--- plugins/quake/tools/quantum-convert/quantum-convert.cpp
+++ plugins/quake/tools/quantum-convert/quantum-convert.cpp
@@ -8,6 +8,10 @@
  * Licensed under the MIT License
  */
 
+#include "mlir/Conversion/MQTDynToQuake/MQTDynToQuake.h"
+#include "mlir/Dialect/MQTDyn/IR/MQTDynDialect.h"
+
+#include <cudaq/Optimizer/Dialect/Quake/QuakeDialect.h>
 #include <mlir/Dialect/Func/Extensions/AllExtensions.h>
 #include <mlir/IR/DialectRegistry.h>
 #include <mlir/InitAllDialects.h>
@@ -16,10 +20,13 @@
 
 int main(const int argc, char** argv) {
   mlir::registerAllPasses();
+  mqt::ir::conversions::registerMQTDynToQuakePasses();
 
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
   mlir::func::registerAllExtensions(registry);
+  registry.insert<mqt::ir::dyn::MQTDynDialect>();
+  registry.insert<quake::QuakeDialect>();
 
   return mlir::asMainReturnCode(
       MlirOptMain(argc, argv, "Quantum conversion driver\n", registry));
