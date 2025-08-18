233e475e1b528f9b863c7650802443dd166cb368
Damian Rovara <damianrovara@gmail.com>
2025-08-18 14:03:03 +0200

    feat(mlir/hybrid-opt): :sparkles: initial implementation of qubit reuse in the most trivial way

--- mlir/lib/Dialect/MQTOpt/Transforms/ReuseQubitsPattern.cpp
+++ mlir/lib/Dialect/MQTOpt/Transforms/ReuseQubitsPattern.cpp
@@ -15,6 +15,7 @@
 #include <cstddef>
 #include <iterator>
 #include <llvm/ADT/STLExtras.h>
+#include <mlir/Dialect/SCF/IR/SCF.h>
 #include <mlir/IR/Block.h>
 #include <mlir/IR/MLIRContext.h>
 #include <mlir/IR/Operation.h>
@@ -30,15 +31,104 @@ namespace mqt::ir::opt {
 /**
  * @brief This pattern is responsible applying qubit reuse.
  */
-struct ReuseQubitsPattern final : mlir::OpRewritePattern<MeasureOp> {
+struct ReuseQubitsPattern final : mlir::OpRewritePattern<AllocQubitOp> {
 
   explicit ReuseQubitsPattern(mlir::MLIRContext* context)
       : OpRewritePattern(context) {}
 
+  /**
+   * @brief Checks if the uses of a qubit allocated by `allocQubit` is disjoint
+   * from the uses of a qubit involved in a deallocation.
+   * @param allocQubit The allocated qubit to check.
+   * @param dealloc The deallocation operation to check against.
+   * @return `true` if the qubits are disjoint, `false` otherwise.
+   */
+  static bool areQubitsDisjoint(mlir::Value allocQubit,
+                                DeallocQubitOp dealloc) {
+    // If traversing the def-use chain from the "alloc" qubit never reaches the
+    // "dealloc" qubit, they are disjoint.
+    llvm::SmallVector<mlir::Operation*> toVisit{allocQubit.getUsers().begin(),
+                                                allocQubit.getUsers().end()};
+    while (!toVisit.empty()) {
+      auto* current = toVisit.back();
+      toVisit.pop_back();
+
+      // If we reach the dealloc operation, the qubits are not disjoint.
+      if (current == dealloc) {
+        return false;
+      }
+
+      if (auto yieldOp = llvm::dyn_cast<mlir::scf::YieldOp>(current)) {
+        // If we reach a yield operation, we continue from the corresponding
+        // `parent`.
+        toVisit.push_back(yieldOp->getParentOp());
+        continue;
+      }
+
+      // Add all users of the current operation to the visit list.
+      for (auto* user : current->getUsers()) {
+        toVisit.push_back(user);
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * @brief Reorders the users of the given operation to ensure that they are
+   * after it.
+   * @param op The operation whose users should be reordered.
+   * @param rewriter The pattern rewriter to use for moving operations.
+   */
+  static void reorderUsers(mlir::Operation* op,
+                           mlir::PatternRewriter& rewriter) {
+    for (auto* user : op->getUsers()) {
+      // Move the user operation after the allocation operation.
+      if (op->isBeforeInBlock(user)) {
+        continue; // Already in the correct order.
+      }
+      rewriter.moveOpAfter(user, op);
+
+      reorderUsers(user, rewriter);
+    }
+  }
+
+  /**
+   * @brief Rewrites the given `AllocQubitOp` and `DeallocQubitOp` to reuse the
+   * qubit instead.
+   *
+   * @param alloc The allocation that will be replaced by qubit reuse.
+   * @param dealloc The deallocation that will be replaced by a reset operation.
+   * @param rewriter The pattern rewriter to use for the rewrite.
+   */
+  static void rewriteForReuse(AllocQubitOp alloc, DeallocQubitOp dealloc,
+                              mlir::PatternRewriter& rewriter) {
+    rewriter.setInsertionPointAfter(dealloc);
+    auto reset = rewriter.replaceOpWithNewOp<ResetOp>(
+        alloc, alloc.getQubit().getType(), dealloc.getQubit());
+    rewriter.eraseOp(dealloc);
+
+    reorderUsers(reset, rewriter);
+  }
+
   mlir::LogicalResult
-  matchAndRewrite(MeasureOp op,
+  matchAndRewrite(AllocQubitOp op,
                   mlir::PatternRewriter& rewriter) const override {
-    return mlir::failure();
+    auto* currentBlock = op->getBlock();
+    auto deallocs = currentBlock->getOps<DeallocQubitOp>();
+    auto reusable = llvm::find_if(deallocs, [&](DeallocQubitOp dealloc) {
+      // Check if the qubit to be deallocated is disjoint from the qubit to be
+      // allocated.
+      return areQubitsDisjoint(op.getQubit(), dealloc);
+    });
+
+    if (reusable == deallocs.end()) {
+      // No reusable qubit found, nothing to do.
+      return mlir::failure();
+    }
+
+    rewriteForReuse(op, *reusable, rewriter);
+    return mlir::success();
   }
 };
 
