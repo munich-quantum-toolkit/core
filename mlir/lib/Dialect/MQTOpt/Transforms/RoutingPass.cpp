68edfe9d84c3d043762339156d7c4c6acaee7ffa
Matthias Reumann <matthias.reumann@tum.de>
2025-08-23 10:49:47 +0200

    Remove "hotqubits"

--- mlir/lib/Dialect/MQTOpt/Transforms/RoutingPass.cpp
+++ mlir/lib/Dialect/MQTOpt/Transforms/RoutingPass.cpp
@@ -14,8 +14,6 @@
 #include <algorithm>
 #include <cstddef>
 #include <cstdint>
-#include <iterator>
-#include <list>
 #include <memory>
 #include <mlir/IR/Attributes.h>
 #include <mlir/IR/Builders.h>
@@ -60,32 +58,31 @@ void random(std::vector<uint64_t>& perm) {
 }
 }; // namespace initial_mapping_funcs
 
-struct BidirectionalMap {
-  using Map = llvm::DenseMap<std::size_t, Value>;
-  using InvMap = llvm::DenseMap<Value, std::size_t>;
+/// @brief Maps from T1 to T2 and from T2 to T1.
+template <class T1, class T2> struct BidirectionalMap {
+  using Map = llvm::DenseMap<T1, T2>;
+  using InvMap = llvm::DenseMap<T2, T1>;
 
-  using MapValueType = Map::value_type;
-
-  // Set bidirectional (index <-> qubit) mapping.
-  void set(const std::size_t i, const Value q) {
-    map_[i] = q;
-    invMap_[q] = i;
+  // Set bidirectional (T1 <-> T2) mapping.
+  void set(const T1 t1, T2 t2) {
+    map_[t1] = t2;
+    invMap_[t2] = t1;
   }
 
-  /// @brief Get qubit from index.
-  [[nodiscard]] Value get(const std::size_t i) { return getForward().at(i); }
+  /// @brief Get T2 from T1.
+  [[nodiscard]] T2 get(const T1 key) { return getForward().at(key); }
 
-  /// @brief Get index from qubit.
-  [[nodiscard]] std::size_t get(const Value q) { return getBackward().at(q); }
+  /// @brief Get T1 from T2.
+  [[nodiscard]] T1 get(const T2 key) { return getBackward().at(key); }
 
-  /// @brief Returns True if @p i is in the forward map.
-  [[nodiscard]] bool contains(const std::size_t i) {
-    return getForward().contains(i);
+  /// @brief Return true if bi-map contains T1 @p key.
+  [[nodiscard]] bool contains(const T1 key) {
+    return getForward().contains(key);
   }
 
-  /// @brief Returns True if @p q is in the backward map.
-  [[nodiscard]] bool contains(const Value q) {
-    return getBackward().contains(q);
+  /// @brief Return true if bi-map contains T2 @p key.
+  [[nodiscard]] bool contains(const T2 key) {
+    return getBackward().contains(key);
   }
 
   /// @brief Get reference of forward (T1 -> T2) map.
@@ -95,12 +92,14 @@ struct BidirectionalMap {
   [[nodiscard]] InvMap& getBackward() { return invMap_; }
 
   /// @brief Erase from bidirectional map.
-  void erase(const std::size_t i, const Value q) {
-    map_.erase(i);
-    invMap_.erase(q);
+  void erase(const T1 t1, const T2 t2) {
+    map_.erase(t1);
+    invMap_.erase(t2);
   }
 
-  /// @brief Returns True if the bidirectional map has no entries.
+  [[nodiscard]] std::size_t size() const { return map_.size(); }
+
+  /// @brief Return true if the bidirectional map has no entries.
   [[nodiscard]] bool empty() const { return map_.empty() && invMap_.empty(); }
 
 private:
@@ -108,23 +107,26 @@ private:
   InvMap invMap_;
 };
 
+/// @brief A QubitPermutation maps indices to qubits and qubits to indices.
+using QubitPermutation = BidirectionalMap<std::size_t, Value>;
+
 template <std::size_t N = 0> class Lookahead {
 private:
   /// @brief A layer consists of a qubit mapping and a set of 2Q gates.
   struct Layer {
-    BidirectionalMap qubits;
+    QubitPermutation p;
     llvm::DenseSet<UnitaryInterface> gates;
   };
 
   /// @brief Replace @p oldQ with @p newQ but keep the same index.
   static void updateMapping(const Value oldQ, const Value newQ,
-                            BidirectionalMap& qubits) {
+                            QubitPermutation& qubits) {
     qubits.set(qubits.get(oldQ), newQ);
     qubits.getBackward().erase(oldQ);
   }
 
   /// @brief Absorb operations that take and return a single qubit.
-  static bool absorbWire(const Value oldQ, BidirectionalMap& qubits) {
+  static bool absorbWire(const Value oldQ, QubitPermutation& qubits) {
     bool hasDeallocated = false;
 
     Value newQ = oldQ;
@@ -156,29 +158,23 @@ private:
     return hasDeallocated;
   }
 
-public:
-  explicit Lookahead(const BidirectionalMap& p0) { init(p0); }
-
-  [[nodiscard]] std::span<const Layer> get() const { return layers; }
-
-private:
   /// @brief Absorb 1Q operations. Remove any deallocated qubits.
-  void absorb(Layer& l) {
-    std::vector<BidirectionalMap::MapValueType> deallocated{};
-    for (const auto& [i, q] : l.qubits.getForward()) {
-      if (absorbWire(q, l.qubits)) {
+  static void absorb(Layer& l) {
+    std::vector<std::pair<std::size_t, Value>> deallocated{};
+    for (const auto& [i, q] : l.p.getForward()) {
+      if (absorbWire(q, l.p)) {
         deallocated.emplace_back(i, q);
       }
     }
 
     for (const auto& [i, q] : deallocated) {
-      l.qubits.erase(i, q);
+      l.p.erase(i, q);
     }
   }
 
   /// @brief Collect 2Q gates of the layer.
-  void collectGates(Layer& l) {
-    for (auto [i, q] : l.qubits.getForward()) {
+  static void collectGates(Layer& l) {
+    for (auto [i, q] : l.p.getForward()) {
       Operation* user = *(q.getUsers().begin());
       UnitaryInterface unitary = dyn_cast<UnitaryInterface>(user);
       assert(unitary);
@@ -193,15 +189,15 @@ private:
 
       auto target = unitary.getInQubits().front();
       auto control = unitary.getAllCtrlInQubits().front();
-      if (l.qubits.contains(target) && l.qubits.contains(control)) {
+      if (l.p.contains(target) && l.p.contains(control)) {
         l.gates.insert(unitary);
       }
     }
   }
 
   /// @brief Return the permutation of the layer after @p l.
-  BidirectionalMap nextPermutation(Layer& l) {
-    BidirectionalMap p = l.qubits;
+  static QubitPermutation nextPermutation(Layer& l) {
+    QubitPermutation pNext = l.p;
 
     for (UnitaryInterface unitary : l.gates) {
       const auto targetIn = unitary.getInQubits().front();
@@ -209,26 +205,33 @@ private:
       const auto controlIn = unitary.getAllCtrlInQubits().front();
       const auto controlOut = unitary.getAllCtrlOutQubits().front();
 
-      Lookahead::updateMapping(targetIn, targetOut, p);
-      Lookahead::updateMapping(controlIn, controlOut, p);
+      Lookahead::updateMapping(targetIn, targetOut, pNext);
+      Lookahead::updateMapping(controlIn, controlOut, pNext);
     }
 
-    return p;
+    return pNext;
   }
 
-  void init(const BidirectionalMap& p0) {
-    layers[0] = Layer{.qubits = p0};
+public:
+  explicit Lookahead(const QubitPermutation& p0) {
+    layers[0].p = p0;
 
     for (std::size_t i = 0; i < layers.size(); ++i) {
-      absorb(layers[i]);
-      collectGates(layers[i]);
+      Lookahead::absorb(layers[i]);
+      Lookahead::collectGates(layers[i]);
 
       if (i < layers.size() - 1) {
-        layers[i + 1].qubits = nextPermutation(layers[i]);
+        layers[i + 1].p = nextPermutation(layers[i]);
       }
     }
   }
 
+  /// @brief Return view of layers.
+  [[nodiscard]] std::span<const Layer> get() const { return layers; }
+
+  explicit operator bool() const { return !layers[0].p.empty(); }
+
+private:
   /// @brief An array of maximally (1 + lookahead) layers.
   std::array<Layer, 1 + N> layers;
 };
@@ -255,12 +258,13 @@ void setModuleTarget(Operation* moduleLike, PatternRewriter& rewriter,
 }
 
 /// @brief Create @p nqubits static qubits in module-like parent.
-void addModuleQubits(Operation* moduleLike, const uint64_t nqubits,
-                     BidirectionalMap& qubits, PatternRewriter& rewriter) {
+void addModuleQubits(Operation* moduleLike, PatternRewriter& rewriter,
+                     const Architecture& target,
+                     llvm::SmallVectorImpl<Value>& qubits) {
   const auto& qubit = QubitType::get(moduleLike->getContext());
-  for (std::size_t i = 0; i < nqubits; ++i) {
+  for (std::size_t i = 0; i < target.nqubits(); ++i) {
     auto qubitOp = rewriter.create<QubitOp>(moduleLike->getLoc(), qubit, i);
-    qubits.set(i, qubitOp.getQubit());
+    qubits[i] = qubitOp.getQubit();
   }
 }
 
@@ -273,13 +277,14 @@ LogicalResult spanAndFold(Operation* base,
   // 1) All extract's are BUNDLED.
   // 2) Qubits are extracted ONCE.
   // 3) All used qubits are extracted before any computation.
+  // 4) Extracts use static indices.
 
-  BidirectionalMap qubits{};                     // Holds index <-> qubit.
-  llvm::BitVector hotQubits(target->nqubits());  // Currently used qubits.
   std::vector<uint64_t> perm(target->nqubits()); // Holds initial layout.
-
   initial_mapping_funcs::identity(perm);
 
+  llvm::SmallVector<Value, 0> qubits(target->nqubits());
+
+  QubitPermutation p{}; // Holds index <-> qubit.
   auto result = base->walk([&](ExtractOp extract) {
     rewriter.setInsertionPoint(extract);
 
@@ -290,28 +295,27 @@ LogicalResult spanAndFold(Operation* base,
     assert(moduleLike);
     if (!moduleLike->hasAttr(attribute_names::TARGET)) {
       setModuleTarget(moduleLike, rewriter, *target);
-      addModuleQubits(moduleLike, target->nqubits(), qubits, rewriter);
+      addModuleQubits(moduleLike, rewriter, *target, qubits);
     }
 
-    const std::optional<uint64_t> indexAttr = extract.getIndexAttr();
-    assert(indexAttr.has_value()); // Assume a static index for now.
+    assert(extract.getIndexAttr().has_value());
+    
+    const std::size_t index = perm[extract.getIndexAttr().value()];
+    const Value dynQ = extract.getOutQubit();
+    const Value statQ = qubits[index];
 
-    const std::size_t idx = perm[indexAttr.value()];
-    const Value dynQ = extract.getOutQubit();     // The dynamic qubit.
-    const Value statQ = qubits.getForward()[idx]; // The static qubit.
-
-    if (hotQubits[idx]) {
+    if (p.contains(index)) {
       return WalkResult(extract.emitOpError()
                         << "extracted the same qubit twice.");
     }
 
-    if (hotQubits.all()) {
+    if (p.size() == qubits.size()) {
       return WalkResult(
           extract.emitOpError()
           << "requires more qubits than the architecture supports");
     }
 
-    hotQubits.set(idx);
+    p.set(index, statQ);
 
     rewriter.replaceAllUsesWith(dynQ, statQ);
     rewriter.replaceAllUsesWith(extract.getOutQreg(), extract.getInQreg());
@@ -326,12 +330,24 @@ LogicalResult spanAndFold(Operation* base,
     //  Mapping circuit until inserts arrive.
     // ---------------------------------------------
 
-    Lookahead<2> lookahead(qubits);
-    for(const auto& l : lookahead.get()) {
-      for(const auto& gate : l.gates) {
-        llvm::outs() << gate->getLoc() << '\n';
+    while (auto lookahead = Lookahead<1>(p)) {
+
+      const auto layers = lookahead.get();
+
+      llvm::outs() << "Current:" << '\n';
+      for (const auto& gate : layers[0].gates) {
+        llvm::outs() << "\t" << gate->getLoc() << '\n';
       }
-      llvm::outs() << "------" << '\n';
+
+      llvm::outs() << "Lookahead:" << '\n';
+      for (const auto& l : layers.subspan(1)) {
+        for (const auto& gate : l.gates) {
+          llvm::outs() << "\t" << gate->getLoc() << '\n';
+        }
+        llvm::outs() << "\t------" << '\n';
+      }
+
+      p = layers[1].p;
     }
 
     return WalkResult::advance();
