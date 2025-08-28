94f500dec9d598c0edc4b32b167364adb54dd63c
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-08-28 09:16:46 +0200

    🐛 Fix python tests, make the ready for optional return value

--- test/python/qdmi/test_fomac.py
+++ test/python/qdmi/test_fomac.py
@@ -10,7 +10,6 @@
 
 from __future__ import annotations
 
-from re import match
 from typing import cast
 
 import pytest
@@ -52,41 +51,41 @@ def device_and_operation(request: pytest.FixtureRequest) -> tuple[Device, Operat
     return device, operation
 
 
-def test_device_name(device: Device) -> None:
+def testdevice_name(device: Device) -> None:
     """Test that the device name is a non-empty string."""
     name = device.name()
     assert isinstance(name, str)
     assert len(name) > 0
 
 
-def test_device_version(device: Device) -> None:
+def testdevice_version(device: Device) -> None:
     """Test that the device version is a non-empty string."""
     version = device.version()
     assert isinstance(version, str)
     assert len(version) > 0
 
 
-def test_device_status(device: Device) -> None:
+def testdevice_status(device: Device) -> None:
     """Test that the device status is a valid DeviceStatus enum member."""
     status = device.status()
     assert isinstance(status, DeviceStatus)
 
 
-def test_device_library_version(device: Device) -> None:
+def testdevice_library_version(device: Device) -> None:
     """Test that the device library version is a non-empty string."""
     lib_version = device.library_version()
     assert isinstance(lib_version, str)
     assert len(lib_version) > 0
 
 
-def test_device_qubits_num(device: Device) -> None:
+def testdevice_qubits_num(device: Device) -> None:
     """Test that the device qubits number is a positive integer."""
     qubits_num = device.qubits_num()
     assert isinstance(qubits_num, int)
     assert qubits_num > 0
 
 
-def test_device_sites(device: Device) -> None:
+def testdevice_sites(device: Device) -> None:
     """Test that the device sites is a non-empty list of Site objects."""
     sites = device.sites()
     assert isinstance(sites, list)
@@ -94,7 +93,7 @@ def test_device_sites(device: Device) -> None:
     assert all(isinstance(site, Site) for site in sites)
 
 
-def test_device_operations(device: Device) -> None:
+def testdevice_operations(device: Device) -> None:
     """Test that the device operations is a non-empty list of Operation objects."""
     operations = device.operations()
     assert isinstance(operations, list)
@@ -102,74 +101,60 @@ def test_device_operations(device: Device) -> None:
     assert all(isinstance(op, Operation) for op in operations)
 
 
-def test_device_coupling_map(device: Device) -> None:
+def testdevice_coupling_map(device: Device) -> None:
     """Test that the device coupling map is a list of tuples of Site objects."""
-    try:
-        cm = device.coupling_map()
+    cm = device.coupling_map()
+    if cm is not None:
         assert isinstance(cm, list)
         assert all(len(pair) == 2 for pair in cm)
         assert all(isinstance(site, Site) for pair in cm for site in pair)
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
-def test_device_needs_calibration(device: Device) -> None:
+def testdevice_needs_calibration(device: Device) -> None:
     """Test that the device needs calibration is an integer."""
-    try:
-        needs_cal = device.needs_calibration()
+    needs_cal = device.needs_calibration()
+    if needs_cal is not None:
         assert isinstance(needs_cal, int)
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
-def test_device_length_unit(device: Device) -> None:
+def testdevice_length_unit(device: Device) -> None:
     """Test that the device length unit is a non-empty string."""
-    try:
-        lu = device.length_unit()
+    lu = device.length_unit()
+    if lu is not None:
         assert isinstance(lu, str)
         assert len(lu) > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
-def test_device_length_scale_factor(device: Device) -> None:
+def testdevice_length_scale_factor(device: Device) -> None:
     """Test that the device length scale factor is a positive float."""
-    try:
-        lsf = device.length_scale_factor()
+    lsf = device.length_scale_factor()
+    if lsf is not None:
         assert isinstance(lsf, float)
         assert lsf > 0.0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
-def test_device_duration_unit(device: Device) -> None:
+def testdevice_duration_unit(device: Device) -> None:
     """Test that the device duration unit is a non-empty string."""
-    try:
-        du = device.duration_unit()
+    du = device.duration_unit()
+    if du is not None:
         assert isinstance(du, str)
         assert len(du) > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
-def test_device_duration_scale_factor(device: Device) -> None:
+def testdevice_duration_scale_factor(device: Device) -> None:
     """Test that the device duration scale factor is a positive float."""
-    try:
-        dsf = device.duration_scale_factor()
+    dsf = device.duration_scale_factor()
+    if dsf is not None:
         assert isinstance(dsf, float)
         assert dsf > 0.0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
-def test_device_min_atom_distance(device: Device) -> None:
+def testdevice_min_atom_distance(device: Device) -> None:
     """Test that the device minimum atom distance is a positive float."""
-    try:
-        mad = device.min_atom_distance()
+    mad = device.min_atom_distance()
+    if mad is not None:
         assert isinstance(mad, int)
         assert mad > 0.0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_index(device_and_site: tuple[Device, Site]) -> None:
@@ -183,129 +168,105 @@ def test_site_index(device_and_site: tuple[Device, Site]) -> None:
 def test_site_t1(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site T1 coherence time is a positive integer."""
     _device, site = device_and_site
-    try:
-        t1 = site.t1()
+    t1 = site.t1()
+    if t1 is not None:
         assert isinstance(t1, int)
         assert t1 > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_t2(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site T2 coherence time is a positive integer."""
     _device, site = device_and_site
-    try:
-        t2 = site.t2()
+    t2 = site.t2()
+    if t2 is not None:
         assert isinstance(t2, int)
         assert t2 > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_name(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site name is a non-empty string."""
     _device, site = device_and_site
-    try:
-        name = site.name()
+    name = site.name()
+    if name is not None:
         assert isinstance(name, str)
         assert len(name) > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_x_coordinate(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site x coordinate is an integer."""
     _device, site = device_and_site
-    try:
-        x = site.x_coordinate()
+    x = site.x_coordinate()
+    if x is not None:
         assert isinstance(x, int)
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_y_coordinate(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site y coordinate is an integer."""
     _device, site = device_and_site
-    try:
-        y = site.y_coordinate()
+    y = site.y_coordinate()
+    if y is not None:
         assert isinstance(y, int)
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_z_coordinate(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site z coordinate is an integer."""
     _device, site = device_and_site
-    try:
-        z = site.z_coordinate()
+    z = site.z_coordinate()
+    if z is not None:
         assert isinstance(z, int)
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_is_zone(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site is_zone is a boolean."""
     _device, site = device_and_site
-    try:
-        iz = site.is_zone()
+    iz = site.is_zone()
+    if iz is not None:
         assert isinstance(iz, bool)
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_x_extent(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site x extent is a positive integer."""
     _device, site = device_and_site
-    try:
-        xe = site.x_extent()
+    xe = site.x_extent()
+    if xe is not None:
         assert isinstance(xe, int)
         assert xe > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_y_extent(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site y extent is a positive integer."""
     _device, site = device_and_site
-    try:
-        ye = site.y_extent()
+    ye = site.y_extent()
+    if ye is not None:
         assert isinstance(ye, int)
         assert ye > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_z_extent(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site z extent is a positive integer."""
     _device, site = device_and_site
-    try:
-        ze = site.z_extent()
+    ze = site.z_extent()
+    if ze is not None:
         assert isinstance(ze, int)
         assert ze > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_module_index(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site module index is a non-negative integer."""
     _device, site = device_and_site
-    try:
-        mi = site.module_index()
+    mi = site.module_index()
+    if mi is not None:
         assert isinstance(mi, int)
         assert mi >= 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_site_submodule_index(device_and_site: tuple[Device, Site]) -> None:
     """Test that the site submodule index is a non-negative integer."""
     _device, site = device_and_site
-    try:
-        smi = site.submodule_index()
+    smi = site.submodule_index()
+    if smi is not None:
         assert isinstance(smi, int)
         assert smi >= 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_name(device_and_operation: tuple[Device, Operation]) -> None:
@@ -319,110 +280,90 @@ def test_operation_name(device_and_operation: tuple[Device, Operation]) -> None:
 def test_operation_qubits_num(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation qubits number is a positive integer."""
     _device, operation = device_and_operation
-    try:
-        qn = operation.qubits_num()
+    qn = operation.qubits_num()
+    if qn is not None:
         assert isinstance(qn, int)
         assert qn > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_parameters_num(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation parameters number is a non-negative integer."""
     _device, operation = device_and_operation
-    try:
-        on = operation.parameters_num()
+    on = operation.parameters_num()
+    if on is not None:
         assert isinstance(on, int)
         assert on >= 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_duration(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation duration is a positive integer."""
     _device, operation = device_and_operation
-    try:
-        dur = operation.duration()
+    dur = operation.duration()
+    if dur is not None:
         assert isinstance(dur, int)
         assert dur > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_fidelity(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation fidelity is a float between 0 and 1."""
     _device, operation = device_and_operation
-    try:
-        fid = operation.fidelity()
+    fid = operation.fidelity()
+    if fid is not None:
         assert isinstance(fid, float)
         assert 0.0 <= fid <= 1.0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_interaction_radius(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation interaction radius is a non-negative integer."""
     _device, operation = device_and_operation
-    try:
-        ir = operation.interaction_radius()
+    ir = operation.interaction_radius()
+    if ir is not None:
         assert isinstance(ir, int)
         assert ir >= 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_blocking_radius(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation blocking radius is a non-negative integer."""
     _device, operation = device_and_operation
-    try:
-        br = operation.blocking_radius()
+    br = operation.blocking_radius()
+    if br is not None:
         assert isinstance(br, int)
         assert br >= 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_idling_fidelity(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation idling fidelity is a float between 0 and 1."""
     _device, operation = device_and_operation
-    try:
-        idf = operation.idling_fidelity()
+    idf = operation.idling_fidelity()
+    if idf is not None:
         assert isinstance(idf, float)
         assert 0.0 <= idf <= 1.0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_is_zoned(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation is_zoned is a boolean."""
     _device, operation = device_and_operation
-    try:
-        iz = operation.is_zoned()
+    iz = operation.is_zoned()
+    if iz is not None:
         assert isinstance(iz, bool)
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_sites(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation sites is a non-empty list of Site objects."""
     device, operation = device_and_operation
-    try:
-        sites = operation.sites()
+    sites = operation.sites()
+    if sites is not None:
         assert isinstance(sites, list)
         assert len(sites) > 0
         assert all(isinstance(site, Site) for site in sites)
         device_sites = device.sites()
         assert all(site in device_sites for site in sites)
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
 
 
 def test_operation_mean_shuttling_speed(device_and_operation: tuple[Device, Operation]) -> None:
     """Test that the operation mean shuttling speed is a positive integer."""
     _device, operation = device_and_operation
-    try:
-        mss = operation.mean_shuttling_speed()
+    mss = operation.mean_shuttling_speed()
+    if mss is not None:
         assert isinstance(mss, int)
         assert mss > 0
-    except RuntimeError as e:
-        assert match(r".*Not supported.*", str(e))
