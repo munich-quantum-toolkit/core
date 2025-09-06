270357876d705000c53141fa0f8731aea897367b
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-09-06 17:27:05 +0300

    🎉 Setup C++ project structure

---
+++ include/mqt-core/na/fomac/Device.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#pragma once
+
+#include "qdmi/FoMaC.hpp"
+
+namespace na::fomac {
+class Device : public qdmi::FoMaC::Device {};
+} // namespace na::fomac
