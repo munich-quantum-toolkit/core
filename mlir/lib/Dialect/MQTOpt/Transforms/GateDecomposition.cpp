46f2aa52929bba6a84dc96b6e4cf33e85f2ff27c
Tamino Bauknecht <dev@tb6.eu>
2025-09-11 15:44:14 +0200

    fix compilation

--- mlir/lib/Dialect/MQTOpt/Transforms/GateDecomposition.cpp
+++ mlir/lib/Dialect/MQTOpt/Transforms/GateDecomposition.cpp
@@ -8,11 +8,11 @@
  * Licensed under the MIT License
  */
 
-#include "mlir/Dialect/Common/Compat.h"
 #include "mlir/Dialect/MQTOpt/Transforms/Passes.h"
 
 #include <mlir/IR/PatternMatch.h>
 #include <mlir/Support/LLVM.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
 #include <utility>
 
 namespace mqt::ir::opt {
@@ -36,7 +36,7 @@ struct GateDecomposition final
     populateGateDecompositionPatterns(patterns);
 
     // Apply patterns in an iterative and greedy manner.
-    if (mlir::failed(APPLY_PATTERNS_GREEDILY(op, std::move(patterns)))) {
+    if (mlir::failed(mlir::applyPatternsGreedily(op, std::move(patterns)))) {
       signalPassFailure();
     }
   }
