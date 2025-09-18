4c5fe4a55e224e0e03417cfaad3210d25c7b0878
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-09-18 13:34:18 +0200

    🐛 Fix QIR runtime

--- src/qir/backend/QIR.cpp
+++ src/qir/backend/QIR.cpp
@@ -10,7 +10,7 @@
 
 #include "qir/backend/QIR.h"
 
-#include "qir/backend/QIR_DD_Backend.hpp"
+#include "qir/backend/Backend.hpp"
 
 #include <algorithm>
 #include <array>
@@ -27,28 +27,26 @@ extern "C" {
 // *** MEASUREMENT RESULTS ***
 Result* __quantum__rt__result_get_zero() {
   // NOLINTNEXTLINE(performance-no-int-to-ptr)
-  return reinterpret_cast<Result*>(qir::QIR_DD_Backend::RESULT_ZERO_ADDRESS);
+  return reinterpret_cast<Result*>(qir::Backend::RESULT_ZERO_ADDRESS);
 }
 
 Result* __quantum__rt__result_get_one() {
   // NOLINTNEXTLINE(performance-no-int-to-ptr)
-  return reinterpret_cast<Result*>(qir::QIR_DD_Backend::RESULT_ONE_ADDRESS);
+  return reinterpret_cast<Result*>(qir::Backend::RESULT_ONE_ADDRESS);
 }
 
 bool __quantum__rt__result_equal(Result* result1, Result* result2) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   return backend.equal(result1, result2);
 }
 
 void __quantum__rt__result_update_reference_count(Result* result,
                                                   const int32_t k) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   // NOLINTBEGIN(performance-no-int-to-ptr)
   if (result != nullptr &&
-      result !=
-          reinterpret_cast<Result*>(qir::QIR_DD_Backend::RESULT_ZERO_ADDRESS) &&
-      result !=
-          reinterpret_cast<Result*>(qir::QIR_DD_Backend::RESULT_ONE_ADDRESS)) {
+      result != reinterpret_cast<Result*>(qir::Backend::RESULT_ZERO_ADDRESS) &&
+      result != reinterpret_cast<Result*>(qir::Backend::RESULT_ONE_ADDRESS)) {
     // NOLINTEND(performance-no-int-to-ptr)
     auto& refcount = backend.deref(result).refcount;
     refcount += k;
@@ -171,7 +169,7 @@ String* __quantum__rt__pauli_to_string(const Pauli p) {
 }
 
 String* __quantum__rt__qubit_to_string(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   return __quantum__rt__int_to_string(
       static_cast<int32_t>(backend.translateAddresses<1>({{qubit}}).front()));
 }
@@ -646,7 +644,7 @@ void __quantum__rt__fail(const String* msg) {
 
 // *** QUANTUM INSTRUCTION SET AND RUNTIME ***
 Qubit* __quantum__rt__qubit_allocate() {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   return backend.qAlloc();
 }
 
@@ -661,7 +659,7 @@ Array* __quantum__rt__qubit_allocate_array(const int64_t n) {
 }
 
 void __quantum__rt__qubit_release(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.qFree(qubit);
 }
 
@@ -679,124 +677,124 @@ void __quantum__rt__qubit_release_array(Array* array) {
 
 // QUANTUM INSTRUCTION SET
 void __quantum__qis__x__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::X, qubit);
 }
 
 void __quantum__qis__y__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::Y, qubit);
 }
 
 void __quantum__qis__z__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::Z, qubit);
 }
 
 void __quantum__qis__h__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::H, qubit);
 }
 
 void __quantum__qis__s__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::S, qubit);
 }
 
 void __quantum__qis__sdg__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::Sdg, qubit);
 }
 
 void __quantum__qis__sx__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::SX, qubit);
 }
 
 void __quantum__qis__sxdg__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::SXdg, qubit);
 }
 
 void __quantum__qis__sqrtx__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::SX, qubit);
 }
 
 void __quantum__qis__sqrtxdg__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::SXdg, qubit);
 }
 
 void __quantum__qis__t__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::T, qubit);
 }
 
 void __quantum__qis__tdg__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::Tdg, qubit);
 }
 
 void __quantum__qis__rx__body(Qubit* qubit, const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::RX, phi, qubit);
 }
 
 void __quantum__qis__ry__body(Qubit* qubit, const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::RY, phi, qubit);
 }
 
 void __quantum__qis__rz__body(Qubit* qubit, const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::RZ, phi, qubit);
 }
 
 void __quantum__qis__p__body(Qubit* qubit, const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::P, phi, qubit);
 }
 
 void __quantum__qis__rxx__body(Qubit* target1, Qubit* target2,
                                const double theta) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::RXX, theta, target1, target2);
 }
 
 void __quantum__qis__u__body(Qubit* qubit, const double theta, const double phi,
                              const double lambda) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::U, theta, phi, lambda, qubit);
 }
 
 void __quantum__qis__u3__body(Qubit* qubit, const double theta,
                               const double phi, const double lambda) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::U, theta, phi, lambda, qubit);
 }
 
 void __quantum__qis__u2__body(Qubit* qubit, const double theta,
                               const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::U2, theta, phi, qubit);
 }
 
 void __quantum__qis__u1__body(Qubit* qubit, const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::P, phi, qubit);
 }
 
 void __quantum__qis__cu1__body(Qubit* control, Qubit* target,
                                const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::P, phi, control, target);
 }
 
 void __quantum__qis__cu3__body(Qubit* control, Qubit* target,
                                const double theta, const double phi,
                                const double lambda) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::U, theta, phi, lambda, control, target);
 }
 
@@ -805,84 +803,84 @@ void __quantum__qis__cnot__body(Qubit* control, Qubit* target) {
 }
 
 void __quantum__qis__cx__body(Qubit* control, Qubit* target) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::X, control, target);
 }
 
 void __quantum__qis__cy__body(Qubit* control, Qubit* target) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::Y, control, target);
 }
 
 void __quantum__qis__cz__body(Qubit* control, Qubit* target) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::Z, control, target);
 }
 
 void __quantum__qis__ch__body(Qubit* control, Qubit* target) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::H, control, target);
 }
 
 void __quantum__qis__swap__body(Qubit* target1, Qubit* target2) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.swap(target1, target2);
 }
 
 void __quantum__qis__cswap__body(Qubit* control, Qubit* target1,
                                  Qubit* target2) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::SWAP, control, target1, target2);
 }
 
 void __quantum__qis__crz__body(Qubit* control, Qubit* target,
                                const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::RZ, phi, control, target);
 }
 
 void __quantum__qis__cry__body(Qubit* control, Qubit* target,
                                const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::RY, phi, control, target);
 }
 
 void __quantum__qis__crx__body(Qubit* control, Qubit* target,
                                const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::RX, phi, control, target);
 }
 
 void __quantum__qis__cp__body(Qubit* control, Qubit* target, const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::P, phi, control, target);
 }
 
 void __quantum__qis__rzz__body(Qubit* target1, Qubit* target2,
                                const double phi) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::RZZ, phi, target1, target2);
 }
 
 void __quantum__qis__ccx__body(Qubit* control1, Qubit* control2,
                                Qubit* target) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::X, control1, control2, target);
 }
 
 void __quantum__qis__ccz__body(Qubit* control1, Qubit* control2,
                                Qubit* target) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.apply(qc::Z, control1, control2, target);
 }
 
 void __quantum__qis__mz__body(Qubit* qubit, Result* result) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.measure(qubit, result);
 }
 
 Result* __quantum__qis__m__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   auto* result = backend.rAlloc();
   __quantum__qis__mz__body(qubit, result);
   return result;
@@ -893,16 +891,16 @@ Result* __quantum__qis__measure__body(Qubit* qubit) {
 }
 
 void __quantum__qis__reset__body(Qubit* qubit) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   backend.reset<1>({qubit});
 }
 
 void __quantum__rt__initialize(char* /*unused*/) {
-  qir::QIR_DD_Backend::getInstance().resetBackend();
+  qir::Backend::getInstance().resetBackend();
 }
 
 bool __quantum__rt__read_result(Result* result) {
-  auto& backend = qir::QIR_DD_Backend::getInstance();
+  auto& backend = qir::Backend::getInstance();
   return backend.deref(result).r;
 }
 
