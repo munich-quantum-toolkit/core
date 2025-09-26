ce9c2c39012d8138fcc5d5786b48f4d7f07b3783
Matthias Reumann <matthias.reumann@tum.de>
2025-09-26 08:28:01 +0200

    Add missing include

--- mlir/lib/Dialect/MQTOpt/Transforms/Transpilation/sc/RoutingVerificationPass.cpp
+++ mlir/lib/Dialect/MQTOpt/Transforms/Transpilation/sc/RoutingVerificationPass.cpp
@@ -21,6 +21,7 @@
 #include <llvm/ADT/TypeSwitch.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
 #include <mlir/Dialect/SCF/IR/SCF.h>
+#include <mlir/IR/BuiltinAttributes.h>
 #include <mlir/IR/BuiltinOps.h>
 #include <mlir/IR/Value.h>
 #include <mlir/IR/Visitors.h>
