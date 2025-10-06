bd65bb69510a4734f40bcf933fd3f0cc9c91742b
Matthias Reumann <matthias.reumann@tum.de>
2025-10-06 17:01:30 +0200

    Fix crawl layerizer bug

--- mlir/include/mlir/Dialect/MQTOpt/Transforms/Transpilation/Layerizer.h
+++ mlir/include/mlir/Dialect/MQTOpt/Transforms/Transpilation/Layerizer.h
@@ -15,6 +15,9 @@
 #include "mlir/Dialect/MQTOpt/Transforms/Transpilation/Layout.h"
 #include "mlir/IR/Value.h"
 
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+
 #include <llvm/ADT/TypeSwitch.h>
 #include <mlir/Dialect/SCF/IR/SCF.h>
 #include <mlir/Support/LLVM.h>
@@ -31,7 +34,7 @@ using Layer = mlir::SmallVector<QubitIndexPair>;
  * @brief A vector of layers.
  * [0]=current, [1]=lookahead (optional), >=2 future layers
  */
-using Layers = mlir::SmallVector<Layer, 2>;
+using Layers = mlir::SmallVector<Layer>;
 
 /**
  * @brief A layerizer divides the circuit into routable sections.
@@ -59,75 +62,82 @@ struct OneOpLayerizer final : LayerizerBase {
  * in parallel, i.e., they don't depend each others results.
  */
 struct CrawlLayerizer final : LayerizerBase {
+  std::size_t nlookahead = 1;
+
   [[nodiscard]] Layers
   layerize([[maybe_unused]] UnitaryInterface op,
            const Layout<QubitIndex>& layout) const override {
     Layout<QubitIndex> copy(layout);
+    Layers layers(1 + nlookahead);
 
-    const auto crawl = [&] {
-      mlir::DenseSet<UnitaryInterface> candidates;
-      for (const mlir::Value in : copy.getHardwareQubits()) {
+    for (Layer& layer : layers) {
+      mlir::DenseSet<UnitaryInterface> visited;
+      mlir::SmallVector<UnitaryInterface> gates;
 
-        mlir::Value curr = in;
-        while (!curr.getUsers().empty()) {
-          mlir::Operation* user = *curr.getUsers().begin();
+      for (const mlir::Value curr : copy.getHardwareQubits()) {
+        mlir::Value next = curr;
+        while (!next.getUsers().empty()) {
 
-          using SwitchResult = std::pair<mlir::Value, bool>;
-          auto [next, stop] =
-              mlir::TypeSwitch<mlir::Operation*, SwitchResult>(user)
+          mlir::Operation* user = *next.getUsers().begin();
+          const bool stop =
+              mlir::TypeSwitch<mlir::Operation*, bool>(user)
                   .Case<mlir::scf::ForOp>([&](auto op) {
-                    const QubitIndex hw = copy.lookupHardwareIndex(curr);
-                    return std::make_pair(op->getResult(hw), false);
+                    next = op->getResult(copy.lookupHardwareIndex(next));
+                    return false;
                   })
                   .Case<mlir::scf::IfOp>([&](auto op) {
-                    const QubitIndex hw = copy.lookupHardwareIndex(curr);
-                    return std::make_pair(op->getResult(hw), false);
+                    next = op->getResult(copy.lookupHardwareIndex(next));
+                    return false;
                   })
                   .Case<ResetOp>([&](auto op) {
-                    return std::make_pair(op.getOutQubit(), false);
+                    next = op.getOutQubit();
+                    return false;
                   })
                   .Case<MeasureOp>([&](auto op) {
-                    return std::make_pair(op.getOutQubit(), false);
+                    next = op.getOutQubit();
+                    return false;
                   })
                   .Case<UnitaryInterface>([&](auto op) {
-                    if (isTwoQubitGate(op)) {
-                      candidates.insert(op);
-                      return std::make_pair(curr, true);
+                    if (mlir::isa<GPhaseOp>(op)) {
+                      return true;
                     }
 
-                    const bool isZeroOp = mlir::isa<GPhaseOp>(op);
-                    return !isZeroOp ? std::make_pair(op.getOutQubits().front(),
-                                                      false)
-                                     : std::make_pair(curr, true);
+                    if (isTwoQubitGate(op)) {
+                      /// If we visit a two-qubit gate twice, it is "ready".
+                      if (visited.contains(op)) {
+                        gates.emplace_back(op);
+                        return true;
+                      }
+
+                      visited.insert(op);
+                      return true;
+                    }
+
+                    next = op.getOutQubits().front();
+                    return false;
                   })
-                  .Default([&](mlir::Operation*) {
-                    return std::make_pair(curr, true);
-                  });
+                  .Default([&](mlir::Operation*) { return true; });
 
           if (stop) {
+            if (next != curr) {
+              copy.remapQubitValue(curr, next);
+            }
             break;
           }
-
-          if (next != curr) {
-            copy.remapQubitValue(curr, next);
-            curr = next;
-          }
         }
       }
 
-      mlir::SmallVector<QubitIndexPair> gates;
-      for (UnitaryInterface op : candidates) {
+      for (const UnitaryInterface op : gates) {
         const auto [in0, in1] = getIns(op);
-        if (copy.contains(in0) && copy.contains(in1)) {
-          gates.push_back(
-              {copy.lookupProgramIndex(in0), copy.lookupProgramIndex(in1)});
-        }
+        const auto [out0, out1] = getOuts(op);
+        layer.emplace_back(copy.lookupProgramIndex(in0),
+                           copy.lookupProgramIndex(in1));
+        copy.remapQubitValue(in0, out0);
+        copy.remapQubitValue(in1, out1);
       }
+    }
 
-      return gates;
-    };
-
-    return {crawl(), crawl()};
+    return layers;
   }
 };
 } // namespace mqt::ir::opt
