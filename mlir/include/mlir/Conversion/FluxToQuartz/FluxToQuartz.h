21811fd242f59269ad0f8f3956008cd00213b6e7
burgholzer <burgholzer@me.com>
2025-10-08 01:37:00 +0200

    🚧 basic conversion infrastructure
    Signed-off-by: burgholzer <burgholzer@me.com>

---
+++ mlir/include/mlir/Conversion/FluxToQuartz/FluxToQuartz.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#pragma once
+
+#include <mlir/Pass/Pass.h> // from @llvm-project
+
+#define GEN_PASS_DECL
+#include "mlir/Conversion/FluxToQuartz/FluxToQuartz.h.inc"
+
+#define GEN_PASS_REGISTRATION
+#include "mlir/Conversion/FluxToQuartz/FluxToQuartz.h.inc"
