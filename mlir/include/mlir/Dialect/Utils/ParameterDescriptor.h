b59374836d0b459dba0ff6e56d2db1fc6974409c
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-10-30 13:19:23 +0100

    Add docstrings to ParameterDescriptor

--- mlir/include/mlir/Dialect/Utils/ParameterDescriptor.h
+++ mlir/include/mlir/Dialect/Utils/ParameterDescriptor.h
@@ -15,11 +15,20 @@
 
 namespace mlir::utils {
 
+/**
+ * @brief Descriptor for a parameter that can be either static or dynamic
+ */
 class ParameterDescriptor {
   mlir::FloatAttr valueAttr;
   mlir::Value valueOperand;
 
 public:
+  /**
+   * @brief Construct a new ParameterDescriptor object
+   *
+   * @param attr Static float attribute (optional)
+   * @param operand Dynamic value operand (optional)
+   */
   ParameterDescriptor(mlir::FloatAttr attr = nullptr,
                       mlir::Value operand = nullptr) {
     assert(!(attr && operand) && "Cannot have both static and dynamic values");
@@ -27,16 +36,44 @@ public:
     valueOperand = operand;
   }
 
+  /**
+   * @brief Check if the parameter is static
+   *
+   * @return true if static, false if dynamic
+   */
   bool isStatic() const { return valueAttr != nullptr; }
+
+  /**
+   * @brief Check if the parameter is dynamic
+   *
+   * @return true if dynamic, false if static
+   */
   bool isDynamic() const { return valueOperand != nullptr; }
 
+  /**
+   * @brief Try to get the double value of the parameter
+   *
+   * @return double value
+   */
   double getValueDouble() const {
     if (isDynamic()) {
       llvm_unreachable("Cannot get double value from dynamic parameter");
     }
     return valueAttr.getValueAsDouble();
   }
+
+  /**
+   * @brief Get the static float attribute
+   *
+   * @return mlir::FloatAttr Static float attribute (nullptr if dynamic)
+   */
   mlir::FloatAttr getValueAttr() const { return valueAttr; }
+
+  /**
+   * @brief Get the dynamic value operand
+   *
+   * @return mlir::Value Dynamic value operand (nullptr if static)
+   */
   mlir::Value getValueOperand() const { return valueOperand; }
 };
 
