744d95a7752a0ba54512efc96eacd5a690634077
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2025-11-21 18:39:52 +0100

    Add inverse operation to Quartz dialect

---
+++ mlir/lib/Dialect/Quartz/IR/Modifiers/InvOp.cpp
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#include "mlir/Dialect/Quartz/IR/QuartzDialect.h"
+#include "mlir/Dialect/Utils/MatrixUtils.h"
+
+#include <cstddef>
+#include <functional>
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/Support/Casting.h>
+#include <llvm/Support/ErrorHandling.h>
+#include <mlir/IR/Builders.h>
+#include <mlir/IR/BuiltinAttributes.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/OperationSupport.h>
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/Support/LLVM.h>
+#include <mlir/Support/LogicalResult.h>
+
+using namespace mlir;
+using namespace mlir::quartz;
+using namespace mlir::utils;
+
+namespace {
+
+/**
+ * @brief Cancel nested inverse modifiers.
+ */
+struct CancelNestedInv final : OpRewritePattern<InvOp> {
+  using OpRewritePattern::OpRewritePattern;
+  LogicalResult matchAndRewrite(InvOp invOp,
+                                PatternRewriter& rewriter) const override {
+    auto innerUnitary = invOp.getBodyUnitary();
+    auto innerInvOp = llvm::dyn_cast<InvOp>(innerUnitary.getOperation());
+    if (!innerInvOp) {
+      return failure();
+    }
+
+    auto innerInnerUnitary = innerInvOp.getBodyUnitary();
+    rewriter.replaceOp(invOp, innerInnerUnitary.getOperation());
+    rewriter.eraseOp(innerInvOp);
+
+    return success();
+  }
+};
+
+} // namespace
+
+void InvOp::build(OpBuilder& odsBuilder, OperationState& odsState,
+                  UnitaryOpInterface bodyUnitary) {
+  const OpBuilder::InsertionGuard guard(odsBuilder);
+  auto* region = odsState.addRegion();
+  auto& block = region->emplaceBlock();
+
+  // Move the unitary op into the block
+  odsBuilder.setInsertionPointToStart(&block);
+  odsBuilder.clone(*bodyUnitary.getOperation());
+  odsBuilder.create<YieldOp>(odsState.location);
+}
+
+void InvOp::build(OpBuilder& odsBuilder, OperationState& odsState,
+                  const std::function<void(OpBuilder&)>& bodyBuilder) {
+  const OpBuilder::InsertionGuard guard(odsBuilder);
+  auto* region = odsState.addRegion();
+  auto& block = region->emplaceBlock();
+  odsBuilder.setInsertionPointToStart(&block);
+  bodyBuilder(odsBuilder);
+  odsBuilder.create<YieldOp>(odsState.location);
+}
+
+UnitaryOpInterface InvOp::getBodyUnitary() {
+  return llvm::dyn_cast<UnitaryOpInterface>(&getBody().front().front());
+}
+
+size_t InvOp::getNumQubits() { return getNumTargets() + getNumControls(); }
+
+size_t InvOp::getNumTargets() { return getBodyUnitary().getNumTargets(); }
+
+size_t InvOp::getNumControls() {
+  return getNumPosControls() + getNumNegControls();
+}
+
+size_t InvOp::getNumPosControls() {
+  return getBodyUnitary().getNumPosControls();
+}
+
+size_t InvOp::getNumNegControls() {
+  return getBodyUnitary().getNumNegControls();
+}
+
+Value InvOp::getQubit(const size_t i) { return getBodyUnitary().getQubit(i); }
+
+Value InvOp::getTarget(const size_t i) { return getBodyUnitary().getTarget(i); }
+
+Value InvOp::getPosControl(const size_t i) {
+  return getBodyUnitary().getPosControl(i);
+}
+
+Value InvOp::getNegControl(const size_t i) {
+  return getBodyUnitary().getNegControl(i);
+}
+
+size_t InvOp::getNumParams() { return getBodyUnitary().getNumParams(); }
+
+Value InvOp::getParameter(const size_t i) {
+  return getBodyUnitary().getParameter(i);
+}
+
+bool InvOp::hasStaticUnitary() { return getBodyUnitary().hasStaticUnitary(); }
+
+DenseElementsAttr InvOp::tryGetStaticMatrix() {
+  return getMatrixAdj(getContext(), getBodyUnitary().tryGetStaticMatrix());
+}
+
+LogicalResult InvOp::verify() {
+  auto& block = getBody().front();
+  if (block.getOperations().size() != 2) {
+    return emitOpError("body region must have exactly two operations");
+  }
+  if (!llvm::isa<UnitaryOpInterface>(block.front())) {
+    return emitOpError(
+        "first operation in body region must be a unitary operation");
+  }
+  if (!llvm::isa<YieldOp>(block.back())) {
+    return emitOpError(
+        "second operation in body region must be a yield operation");
+  }
+  llvm::SmallPtrSet<Value, 4> uniqueQubits;
+  auto bodyUnitary = getBodyUnitary();
+  const auto numQubits = bodyUnitary.getNumQubits();
+  for (size_t i = 0; i < numQubits; i++) {
+    if (!uniqueQubits.insert(bodyUnitary.getQubit(i)).second) {
+      return emitOpError("duplicate qubit found");
+    }
+  }
+  return success();
+}
+
+void InvOp::getCanonicalizationPatterns(RewritePatternSet& results,
+                                        MLIRContext* context) {
+  results.add<CancelNestedInv>(context);
+}
