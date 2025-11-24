dff917d006fec66b9757cb0eff90fdaf1fa62f64
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-11-24 16:25:40 +0100

    Modularize modifiers

---
+++ mlir/lib/Dialect/Flux/IR/Modifiers/CtrlOp.cpp
@@ -0,0 +1,273 @@
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#include "mlir/Dialect/Flux/FluxUtils.h"
+#include "mlir/Dialect/Flux/IR/FluxDialect.h"
+#include "mlir/Dialect/Utils/MatrixUtils.h"
+
+#include <mlir/IR/BuiltinAttributes.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/OperationSupport.h>
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/Support/LogicalResult.h>
+
+using namespace mlir;
+using namespace mlir::flux;
+using namespace mlir::utils;
+
+namespace {
+
+/**
+ * @brief Merge nested control modifiers into a single one.
+ */
+struct MergeNestedCtrl final : OpRewritePattern<CtrlOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(CtrlOp op,
+                                PatternRewriter& rewriter) const override {
+    auto bodyUnitary = op.getBodyUnitary();
+    auto bodyCtrlOp = llvm::dyn_cast<CtrlOp>(bodyUnitary.getOperation());
+    if (!bodyCtrlOp) {
+      return failure();
+    }
+
+    // Merge controls
+    SmallVector<Value> newControls(op.getControlsIn());
+    for (const auto control : bodyCtrlOp.getControlsIn()) {
+      newControls.push_back(control);
+    }
+
+    rewriter.replaceOpWithNewOp<CtrlOp>(op, newControls, op.getTargetsIn(),
+                                        bodyCtrlOp.getBodyUnitary());
+    rewriter.eraseOp(bodyCtrlOp);
+
+    return success();
+  }
+};
+
+/**
+ * @brief Remove control modifiers without controls.
+ */
+struct RemoveTrivialCtrl final : OpRewritePattern<CtrlOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(CtrlOp op,
+                                PatternRewriter& rewriter) const override {
+    if (op.getNumControls() > 0) {
+      return failure();
+    }
+    rewriter.replaceOp(op, op.getBodyUnitary());
+    return success();
+  }
+};
+
+/**
+ * @brief Inline controlled identity operations.
+ */
+struct CtrlInlineId final : OpRewritePattern<CtrlOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(CtrlOp op,
+                                PatternRewriter& rewriter) const override {
+    if (!llvm::isa<IdOp>(op.getBodyUnitary().getOperation())) {
+      return failure();
+    }
+
+    auto idOp = rewriter.create<IdOp>(op.getLoc(), op.getTargetsIn().front());
+
+    SmallVector<Value> newOperands;
+    newOperands.reserve(op.getNumControls() + 1);
+    newOperands.append(op.getControlsIn().begin(), op.getControlsIn().end());
+    newOperands.push_back(idOp.getQubitOut());
+    rewriter.replaceOp(op, newOperands);
+
+    return success();
+  }
+};
+
+} // namespace
+
+void CtrlOp::build(OpBuilder& odsBuilder, OperationState& odsState,
+                   const ValueRange controls, const ValueRange targets,
+                   UnitaryOpInterface bodyUnitary) {
+  build(odsBuilder, odsState, controls, targets);
+  auto& block = odsState.regions.front()->emplaceBlock();
+
+  // Move the unitary op into the block
+  const OpBuilder::InsertionGuard guard(odsBuilder);
+  odsBuilder.setInsertionPointToStart(&block);
+  auto* op = odsBuilder.clone(*bodyUnitary.getOperation());
+  odsBuilder.create<YieldOp>(odsState.location, op->getResults());
+}
+
+void CtrlOp::build(
+    OpBuilder& odsBuilder, OperationState& odsState, const ValueRange controls,
+    const ValueRange targets,
+    const std::function<ValueRange(OpBuilder&, ValueRange)>& bodyBuilder) {
+  build(odsBuilder, odsState, controls, targets);
+  auto& block = odsState.regions.front()->emplaceBlock();
+
+  // Move the unitary op into the block
+  const OpBuilder::InsertionGuard guard(odsBuilder);
+  odsBuilder.setInsertionPointToStart(&block);
+  auto targetsOut = bodyBuilder(odsBuilder, targets);
+  odsBuilder.create<YieldOp>(odsState.location, targetsOut);
+}
+
+UnitaryOpInterface CtrlOp::getBodyUnitary() {
+  return llvm::dyn_cast<UnitaryOpInterface>(&getBody().front().front());
+}
+
+size_t CtrlOp::getNumQubits() { return getNumTargets() + getNumControls(); }
+
+size_t CtrlOp::getNumTargets() { return getTargetsIn().size(); }
+
+size_t CtrlOp::getNumControls() {
+  return getNumPosControls() + getNumNegControls();
+}
+
+size_t CtrlOp::getNumPosControls() { return getControlsIn().size(); }
+
+size_t CtrlOp::getNumNegControls() {
+  return getBodyUnitary().getNumNegControls();
+}
+
+Value CtrlOp::getInputQubit(const size_t i) {
+  const auto numPosControls = getNumPosControls();
+  if (i < numPosControls) {
+    return getControlsIn()[i];
+  }
+  if (numPosControls <= i && i < getNumQubits()) {
+    return getBodyUnitary().getInputQubit(i - numPosControls);
+  }
+  llvm::report_fatal_error("Invalid qubit index");
+}
+
+Value CtrlOp::getOutputQubit(const size_t i) {
+  const auto numPosControls = getNumPosControls();
+  if (i < numPosControls) {
+    return getControlsOut()[i];
+  }
+  if (numPosControls <= i && i < getNumQubits()) {
+    return getBodyUnitary().getOutputQubit(i - numPosControls);
+  }
+  llvm::report_fatal_error("Invalid qubit index");
+}
+
+Value CtrlOp::getInputTarget(const size_t i) { return getTargetsIn()[i]; }
+
+Value CtrlOp::getOutputTarget(const size_t i) { return getTargetsOut()[i]; }
+
+Value CtrlOp::getInputPosControl(const size_t i) { return getControlsIn()[i]; }
+
+Value CtrlOp::getOutputPosControl(const size_t i) {
+  return getControlsOut()[i];
+}
+
+Value CtrlOp::getInputNegControl(const size_t i) {
+  return getBodyUnitary().getInputNegControl(i);
+}
+
+Value CtrlOp::getOutputNegControl(const size_t i) {
+  return getBodyUnitary().getOutputNegControl(i);
+}
+
+Value CtrlOp::getInputForOutput(const Value output) {
+  for (size_t i = 0; i < getNumPosControls(); ++i) {
+    if (output == getControlsOut()[i]) {
+      return getControlsIn()[i];
+    }
+  }
+  for (size_t i = 0; i < getNumTargets(); ++i) {
+    if (output == getTargetsOut()[i]) {
+      return getTargetsIn()[i];
+    }
+  }
+  llvm::report_fatal_error("Given qubit is not an output of the operation");
+}
+
+Value CtrlOp::getOutputForInput(const Value input) {
+  for (size_t i = 0; i < getNumPosControls(); ++i) {
+    if (input == getControlsIn()[i]) {
+      return getControlsOut()[i];
+    }
+  }
+  for (size_t i = 0; i < getNumTargets(); ++i) {
+    if (input == getTargetsIn()[i]) {
+      return getTargetsOut()[i];
+    }
+  }
+  llvm::report_fatal_error("Given qubit is not an input of the operation");
+}
+
+size_t CtrlOp::getNumParams() { return getBodyUnitary().getNumParams(); }
+
+bool CtrlOp::hasStaticUnitary() { return getBodyUnitary().hasStaticUnitary(); }
+
+Value CtrlOp::getParameter(const size_t i) {
+  return getBodyUnitary().getParameter(i);
+}
+
+DenseElementsAttr CtrlOp::tryGetStaticMatrix() {
+  return getMatrixCtrl(getContext(), getNumPosControls(),
+                       getBodyUnitary().tryGetStaticMatrix());
+}
+
+LogicalResult CtrlOp::verify() {
+  auto& block = getBody().front();
+  if (block.getOperations().size() != 2) {
+    return emitOpError("body region must have exactly two operations");
+  }
+  if (!llvm::isa<UnitaryOpInterface>(block.front())) {
+    return emitOpError(
+        "first operation in body region must be a unitary operation");
+  }
+  if (!llvm::isa<YieldOp>(block.back())) {
+    return emitOpError(
+        "second operation in body region must be a yield operation");
+  }
+  // The yield operation must yield as many values as there are targets
+  if (block.back().getNumOperands() != getNumTargets()) {
+    return emitOpError("yield operation must yield ")
+           << getNumTargets() << " values, but found "
+           << block.back().getNumOperands();
+  }
+
+  SmallPtrSet<Value, 4> uniqueQubitsIn;
+  for (const auto& control : getControlsIn()) {
+    if (!uniqueQubitsIn.insert(control).second) {
+      return emitOpError("duplicate control qubit found");
+    }
+  }
+  auto bodyUnitary = getBodyUnitary();
+  const auto numQubits = bodyUnitary.getNumQubits();
+  for (size_t i = 0; i < numQubits; i++) {
+    if (!uniqueQubitsIn.insert(bodyUnitary.getInputQubit(i)).second) {
+      return emitOpError("duplicate qubit found");
+    }
+  }
+  SmallPtrSet<Value, 4> uniqueQubitsOut;
+  for (const auto& control : getControlsOut()) {
+    if (!uniqueQubitsOut.insert(control).second) {
+      return emitOpError("duplicate control qubit found");
+    }
+  }
+  for (size_t i = 0; i < numQubits; i++) {
+    if (!uniqueQubitsOut.insert(bodyUnitary.getOutputQubit(i)).second) {
+      return emitOpError("duplicate qubit found");
+    }
+  }
+  return success();
+}
+
+void CtrlOp::getCanonicalizationPatterns(RewritePatternSet& results,
+                                         MLIRContext* context) {
+  results.add<MergeNestedCtrl, RemoveTrivialCtrl, CtrlInlineId>(context);
+}
