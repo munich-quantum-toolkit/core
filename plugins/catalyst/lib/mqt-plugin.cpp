3e21e5e195d76c55d0172659103711652561091f
flowerthrower <flowerthrower@users.noreply.github.com>
2025-11-26 14:06:07 +0100

    🧹 Remove redundant licensing information from mqt-plugin.cpp

--- plugins/catalyst/lib/mqt-plugin.cpp
+++ plugins/catalyst/lib/mqt-plugin.cpp
@@ -8,14 +8,6 @@
  * Licensed under the MIT License
  */
 
-//===- mqt-plugin.cpp ------------------------------------*- C++ -*-===//
-//
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #include "mlir/Conversion/CatalystQuantumToMQTOpt/CatalystQuantumToMQTOpt.h"
 #include "mlir/Conversion/MQTOptToCatalystQuantum/MQTOptToCatalystQuantum.h"
 #include "mlir/Dialect/MQTOpt/IR/MQTOptDialect.h"
