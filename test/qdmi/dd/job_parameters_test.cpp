c1efd650c099e470a30f602e8c46c41aa51f8fa2
burgholzer <burgholzer@me.com>
2025-12-01 00:33:26 +0100

    🩹 adjust test based on new supported feature
    Signed-off-by: burgholzer <burgholzer@me.com>

--- test/qdmi/dd/job_parameters_test.cpp
+++ test/qdmi/dd/job_parameters_test.cpp
@@ -74,7 +74,14 @@ TEST(JobParameters, SetAndQueryBasics) {
 
   EXPECT_EQ(MQT_DDSIM_QDMI_device_job_query_property(
                 j.job, QDMI_DEVICE_JOB_PROPERTY_PROGRAM, 0, nullptr, &size),
-            QDMI_ERROR_NOTSUPPORTED);
+            QDMI_SUCCESS);
+  EXPECT_EQ(size, strlen(qdmi_test::QASM3_BELL_SAMPLING) + 1);
+  std::string program(size - 1, '\0');
+  EXPECT_EQ(MQT_DDSIM_QDMI_device_job_query_property(
+                j.job, QDMI_DEVICE_JOB_PROPERTY_PROGRAM, size, program.data(),
+                nullptr),
+            QDMI_SUCCESS);
+  EXPECT_EQ(program, qdmi_test::QASM3_BELL_SAMPLING);
 }
 
 TEST(JobParameters, ProgramFormatSupport) {
