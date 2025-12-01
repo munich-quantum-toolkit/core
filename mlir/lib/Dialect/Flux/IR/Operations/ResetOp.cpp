b5bc9c4dfb237f6ffbc2581b2070a0ec5e8ce750
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-12-01 18:26:27 +0100

    Further modularize operations

---
+++ mlir/lib/Dialect/Flux/IR/Operations/ResetOp.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#include "mlir/Dialect/Flux/IR/FluxDialect.h"
+
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/OperationSupport.h>
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/Support/LogicalResult.h>
+
+using namespace mlir;
+using namespace mlir::flux;
+
+namespace {
+
+/**
+ * @brief Remove reset operations that immediately follow an allocation.
+ */
+struct RemoveResetAfterAlloc final : OpRewritePattern<ResetOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ResetOp op,
+                                PatternRewriter& rewriter) const override {
+    // Check if the predecessor is an AllocOp
+    if (auto allocOp = op.getQubitIn().getDefiningOp<AllocOp>(); !allocOp) {
+      return failure();
+    }
+
+    // Remove the ResetOp
+    rewriter.replaceOp(op, op.getQubitIn());
+    return success();
+  }
+};
+
+} // namespace
+
+void ResetOp::getCanonicalizationPatterns(RewritePatternSet& results,
+                                          MLIRContext* context) {
+  results.add<RemoveResetAfterAlloc>(context);
+}
