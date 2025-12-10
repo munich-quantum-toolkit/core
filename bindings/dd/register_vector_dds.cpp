d65c4bd408f417b449c94968b3e3092f18db703d
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-12-10 11:55:56 +0100

    Include memory

--- bindings/dd/register_vector_dds.cpp
+++ bindings/dd/register_vector_dds.cpp
@@ -16,6 +16,7 @@
 #include <cmath>
 #include <complex>
 #include <cstddef>
+#include <memory>
 #include <nanobind/nanobind.h>
 #include <nanobind/ndarray.h>
 #include <nanobind/stl/complex.h> // NOLINT(misc-include-cleaner)
