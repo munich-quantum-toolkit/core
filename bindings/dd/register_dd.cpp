6c968f3244ab7ef1586b55b3b3b6df48c26a7513
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-12-12 14:33:21 +0100

    Fix install-time stub generation

--- bindings/dd/register_dd.cpp
+++ bindings/dd/register_dd.cpp
@@ -41,6 +41,8 @@ using Matrix = nb::ndarray<nb::numpy, std::complex<dd::fp>, nb::ndim<2>>;
 Matrix getMatrix(const dd::mEdge& m, size_t numQubits, dd::fp threshold = 0.);
 
 NB_MODULE(MQT_CORE_MODULE_NAME, m) {
+  nb::module_::import_("mqt.core.ir");
+
   // Vector Decision Diagrams
   registerVectorDDs(m);
 
