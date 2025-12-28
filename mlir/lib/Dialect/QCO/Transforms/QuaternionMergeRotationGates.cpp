ee6025945e70e6f375587c1f05ec3a3f1129138b
Anatol Bussejahn <anatol.bussejahn@tum.de>
2025-12-28 21:22:43 +0100

    Boilerplate for Quaternion-Merge-Pass in QCO

---
+++ mlir/lib/Dialect/QCO/Transforms/QuaternionMergeRotationGates.cpp
@@ -0,0 +1,40 @@
+
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#include "mlir/Dialect/QCO/Transforms/Passes.h"
+
+#include <iostream>
+#include <mlir/Dialect/Math/IR/Math.h>
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/Support/LLVM.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+#include <utility>
+
+namespace mlir::qco {
+
+#define GEN_PASS_DEF_MERGEROTATIONGATES
+#include "mlir/Dialect/QCO/Transforms/Passes.h.inc"
+
+/**
+ * @brief This pattern attempts to merge consecutive rotation gates by using
+ * quaternions
+ */
+struct MergeRotationGates final
+    : impl::MergeRotationGatesBase<MergeRotationGates> {
+  using impl::MergeRotationGatesBase<
+      MergeRotationGates>::MergeRotationGatesBase;
+
+  void runOnOperation() override {
+    // TODO implement pass here
+  }
+};
+
+} // namespace mlir::qco
