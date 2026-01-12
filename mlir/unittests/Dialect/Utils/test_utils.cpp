d28f4bab1b44282f405a41a14c6e3fe2c275ed27
Tamino Bauknecht <28907748+taminob@users.noreply.github.com>
2026-01-12 21:18:30 +0100

    ♻️ Refactor getStaticParameter to valueToDouble (#1443)
    ## Description
    
    This PR reduces the current code duplication of `getStaticParameter`
    which is used in both `QC` and `QCO`.
    It additionally adds the conversion to a standard C++ double type since
    there is no other use of the function in the code base so far.
    
    An alternative to the currently returned `std::optional` would be a
    `llvm::FailureOr<double>` which is simply a subclass of `std::optional`.
    However, it is in my opinion slightly more annoying to use since it
    requires `llvm::succeeded` and `llvm::failed` to check the result
    instead of the implicit conversion to bool available in `std::optional`.
    This is meant to make it slightly safer to use, but makes the code also
    more verbose and less convenient which is why I decided against using
    it. `std::optional` is an established standard C++ tool and people
    usually know how to handle it correctly which cannot be said for
    `llvm::FailureOr<double>`.
    
    Required for #1426
    
    ## Checklist:
    
    <!---
    This checklist serves as a reminder of a couple of things that ensure
    your pull request will be merged swiftly.
    -->
    
    - [ ] The pull request only contains commits that are focused and
    relevant to this change.
    - [ ] I have added appropriate tests that cover the new/changed
    functionality.
    - [ ] I have updated the documentation to reflect these changes.
    - [ ] I have added entries to the changelog for any noteworthy
    additions, changes, fixes, or removals.
    - [ ] I have added migration instructions to the upgrade guide (if
    needed).
    - [ ] The changes follow the project's style guidelines and introduce no
    new warnings.
    - [ ] The changes are fully tested and pass the CI checks.
    - [ ] I have reviewed my own code changes.
    
    ---------
    
    Signed-off-by: Tamino Bauknecht <28907748+taminob@users.noreply.github.com>
    Co-authored-by: Lukas Burgholzer <burgholzer@me.com>

---
+++ mlir/unittests/Dialect/Utils/test_utils.cpp
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2023 - 2026 Chair for Design Automation, TUM
+ * Copyright (c) 2025 - 2026 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#include "mlir/Dialect/Utils/Utils.h"
+
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <limits>
+#include <llvm/ADT/APInt.h>
+#include <llvm/ADT/SmallVector.h>
+#include <memory>
+#include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/IR/Builders.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/Operation.h>
+#include <mlir/IR/Value.h>
+
+using namespace mlir;
+
+class UtilsTest : public ::testing::Test {
+protected:
+  MLIRContext context;
+  std::unique_ptr<OpBuilder> builder;
+  std::unique_ptr<Location> loc;
+
+  void SetUp() override {
+    context.loadDialect<arith::ArithDialect>();
+
+    builder = std::make_unique<OpBuilder>(&context);
+    loc = std::make_unique<Location>(builder->getUnknownLoc());
+  }
+
+  arith::AddFOp createAddition(double a, double b) {
+    auto firstOperand =
+        builder->create<arith::ConstantOp>(*loc, builder->getF64FloatAttr(a));
+    auto secondOperand =
+        builder->create<arith::ConstantOp>(*loc, builder->getF64FloatAttr(b));
+    return builder->create<arith::AddFOp>(*loc, firstOperand, secondOperand);
+  }
+};
+
+TEST_F(UtilsTest, valueToDouble) {
+  constexpr double expectedValue = 1.234;
+  auto op = builder->create<arith::ConstantOp>(
+      *loc, builder->getF64FloatAttr(expectedValue));
+  ASSERT_TRUE(op);
+
+  auto value = op.getResult();
+  auto stdValue = utils::valueToDouble(value);
+  ASSERT_TRUE(stdValue.has_value());
+  EXPECT_DOUBLE_EQ(stdValue.value(), expectedValue);
+}
+
+TEST_F(UtilsTest, valueToDoubleCastFromInteger) {
+  constexpr int expectedValue = 42;
+  auto op = builder->create<arith::ConstantOp>(
+      *loc, builder->getI32IntegerAttr(expectedValue));
+  ASSERT_TRUE(op);
+
+  auto value = op.getResult();
+  auto stdValue = utils::valueToDouble(value);
+  ASSERT_TRUE(stdValue.has_value());
+  EXPECT_DOUBLE_EQ(stdValue.value(), expectedValue);
+}
+
+TEST_F(UtilsTest, valueToDoubleCastFromNegativeInteger) {
+  constexpr int expectedValue = -123;
+  auto op = builder->create<arith::ConstantOp>(
+      *loc, builder->getSI32IntegerAttr(expectedValue));
+  ASSERT_TRUE(op);
+
+  auto value = op.getResult();
+  auto stdValue = utils::valueToDouble(value);
+  ASSERT_TRUE(stdValue.has_value());
+  EXPECT_DOUBLE_EQ(stdValue.value(), expectedValue);
+}
+
+TEST_F(UtilsTest, valueToDoubleCastFromMaxUnsignedInteger) {
+  constexpr auto expectedValue = std::numeric_limits<uint64_t>::max();
+  constexpr auto bitCount = 64;
+  auto op = builder->create<arith::ConstantOp>(
+      *loc, builder->getIntegerAttr(builder->getIntegerType(bitCount, false),
+                                    llvm::APInt::getMaxValue(bitCount)));
+  ASSERT_TRUE(op);
+
+  auto value = op.getResult();
+  auto stdValue = utils::valueToDouble(value);
+  ASSERT_TRUE(stdValue.has_value());
+  // cast to double will lose precision, but difference to maximum value of
+  // int64_t is large enough that the check still makes sense
+  EXPECT_DOUBLE_EQ(stdValue.value(), static_cast<double>(expectedValue));
+}
+
+TEST_F(UtilsTest, valueToDoubleWrongType) {
+  auto op =
+      builder->create<arith::ConstantOp>(*loc, builder->getStringAttr("test"));
+  ASSERT_TRUE(op);
+
+  auto value = op.getResult();
+  auto stdValue = utils::valueToDouble(value);
+  EXPECT_FALSE(stdValue.has_value());
+}
+
+TEST_F(UtilsTest, valueToDoubleNonStaticValue) {
+  auto op = createAddition(9.5, 21.5);
+  ASSERT_TRUE(op);
+
+  auto value = op.getResult();
+  auto stdValue = utils::valueToDouble(value);
+  EXPECT_FALSE(stdValue.has_value());
+}
+
+TEST_F(UtilsTest, valueToDoubleFoldedConstant) {
+  auto op = createAddition(1.5, 2.0);
+  ASSERT_TRUE(op);
+
+  llvm::SmallVector<Value> tmp;
+  llvm::SmallVector<Operation*> newConstants;
+  ASSERT_TRUE(builder->tryFold(op, tmp, &newConstants).succeeded());
+  ASSERT_EQ(newConstants.size(), 1);
+  auto value = newConstants[0]->getResult(0);
+  auto stdValue = utils::valueToDouble(value);
+  ASSERT_TRUE(stdValue.has_value());
+  EXPECT_DOUBLE_EQ(stdValue.value(), 3.5);
+}
