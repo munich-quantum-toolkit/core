be7ad3ff854f1f0b76f61829b34e3d98f37b15e2
Tamino Bauknecht <dev@tb6.eu>
2026-01-14 11:05:03 +0100

    Revert "replace std::numbers by llvm::numbers because of windows+osx CI"
    This reverts commit 6caa04b9f73675a36a77b14dec1e23b5a27ca177.

--- mlir/lib/Dialect/QCO/IR/Operations/StandardGates/ECROp.cpp
+++ mlir/lib/Dialect/QCO/IR/Operations/StandardGates/ECROp.cpp
@@ -11,7 +11,6 @@
 #include "mlir/Dialect/QCO/IR/QCODialect.h"
 #include "mlir/Dialect/QCO/QCOUtils.h"
 
-#include <llvm/Support/MathExtras.h>
 #include <mlir/IR/MLIRContext.h>
 #include <mlir/IR/OperationSupport.h>
 #include <mlir/IR/PatternMatch.h>
@@ -45,8 +44,8 @@ Eigen::Matrix4cd ECROp::getUnitaryMatrix() {
   using namespace std::complex_literals;
 
   const auto m0 = 0i;
-  const auto m1 = std::complex<double>{1.0 / llvm::numbers::sqrt2};
-  const auto mi = std::complex<double>{0.0, 1.0 / llvm::numbers::sqrt2};
+  const auto m1 = std::complex<double>{1.0 / std::numbers::sqrt2};
+  const auto mi = std::complex<double>{0.0, 1.0 / std::numbers::sqrt2};
   return Eigen::Matrix4cd{{m0, m0, m1, mi},   // row 0
                           {m0, m0, mi, m1},   // row 1
                           {m1, -mi, m0, m0},  // row 2
