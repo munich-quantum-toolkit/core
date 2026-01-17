e1a8512e29a4339ce373ee6f2b9e15ee3ce97d49
Li-ming Bao <ge93vof@tum.de>
2026-01-17 14:23:27 +0100

    add missing header

--- mlir/lib/Conversion/QCOToQC/QCOToQC.cpp
+++ mlir/lib/Conversion/QCOToQC/QCOToQC.cpp
@@ -14,6 +14,7 @@
 #include "mlir/Dialect/QCO/IR/QCODialect.h"
 
 #include <cstddef>
+#include <iterator>
 #include <llvm/ADT/STLExtras.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
 #include <mlir/Dialect/Func/Transforms/FuncConversions.h>
