045857a3c592be2e21fc8c9c65d878998d07ab8e
Anatol Bussejahn <anatol.bussejahn@tum.de>
2026-01-21 20:57:17 +0100

    add more tests

--- mlir/unittests/Dialect/QCO/Transforms/test_qco_quaternion_merge.cpp
+++ mlir/unittests/Dialect/QCO/Transforms/test_qco_quaternion_merge.cpp
@@ -216,6 +216,169 @@ TEST_F(QCOQuaternionMergeTest, quaternionMergeRZUGates) {
   EXPECT_EQ(countOps<RZOp>(), 0);
   EXPECT_EQ(countOps<UOp>(), 1);
 }
+
+// ##################################################
+// # Not Merging Tests
+// ##################################################
+
+// RX->RX should not merge
+TEST_F(QCOQuaternionMergeTest, quaternionNoMergeRXRXGates) {
+  ASSERT_TRUE(testGateMerge({{RXOp::getOperationName(), {1.}},
+                             {RXOp::getOperationName(), {1.}}})
+                  .succeeded());
+  EXPECT_EQ(countOps<UOp>(), 0);
+  EXPECT_EQ(countOps<RXOp>(), 2);
+}
+
+// RY->RY should not merge
+TEST_F(QCOQuaternionMergeTest, quaternionNoMergeRYRYGates) {
+  ASSERT_TRUE(testGateMerge({{RYOp::getOperationName(), {1.}},
+                             {RYOp::getOperationName(), {1.}}})
+                  .succeeded());
+  EXPECT_EQ(countOps<UOp>(), 0);
+  EXPECT_EQ(countOps<RYOp>(), 2);
+}
+
+// RZ->RZ should not merge
+TEST_F(QCOQuaternionMergeTest, quaternionNoMergeRZRZGates) {
+  ASSERT_TRUE(testGateMerge({{RZOp::getOperationName(), {1.}},
+                             {RZOp::getOperationName(), {1.}}})
+                  .succeeded());
+  EXPECT_EQ(countOps<UOp>(), 0);
+  EXPECT_EQ(countOps<RZOp>(), 2);
+}
+
+// single RX should not convert to U
+TEST_F(QCOQuaternionMergeTest, quaternionNoMergeSingleRXGate) {
+  ASSERT_TRUE(testGateMerge({{RXOp::getOperationName(), {1.}}}).succeeded());
+  EXPECT_EQ(countOps<UOp>(), 0);
+  EXPECT_EQ(countOps<RXOp>(), 1);
+}
+
+// single RY should not convert to U
+TEST_F(QCOQuaternionMergeTest, quaternionNoMergeSingleRYGate) {
+  ASSERT_TRUE(testGateMerge({{RYOp::getOperationName(), {1.}}}).succeeded());
+  EXPECT_EQ(countOps<UOp>(), 0);
+  EXPECT_EQ(countOps<RYOp>(), 1);
+}
+
+// single RZ should not convert to U
+TEST_F(QCOQuaternionMergeTest, quaternionNoMergeSingleRZGate) {
+  ASSERT_TRUE(testGateMerge({{RZOp::getOperationName(), {1.}}}).succeeded());
+  EXPECT_EQ(countOps<UOp>(), 0);
+  EXPECT_EQ(countOps<RZOp>(), 1);
+}
+
+// Gates on different qubits should not merge
+TEST_F(QCOQuaternionMergeTest, dontMergeGatesFromDifferentQubits) {
+  auto q = builder.allocQubitRegister(2);
+
+  Value qubit1 = q[0];
+  Value qubit2 = q[1];
+  builder.rx(1.0, qubit1);
+  builder.rx(1.0, qubit2);
+  module = builder.finalize();
+
+  ASSERT_TRUE(runMergePass(module.get()).succeeded());
+  EXPECT_EQ(countOps<RXOp>(), 2);
+}
+
+// Non-consecutive gates should not merge
+TEST_F(QCOQuaternionMergeTest, dontMergeNonConsecutiveGates) {
+  auto q = builder.allocQubitRegister(1);
+
+  auto q1 = builder.rx(1.0, q[0]);
+  auto q2 = builder.h(q1);
+  builder.ry(1.0, q2);
+
+  module = builder.finalize();
+
+  ASSERT_TRUE(runMergePass(module.get()).succeeded());
+  EXPECT_EQ(countOps<RXOp>(), 1);
+  EXPECT_EQ(countOps<HOp>(), 1);
+  EXPECT_EQ(countOps<RYOp>(), 1);
+}
+
+// ##################################################
+// # Special Cases Tests
+// ##################################################
+
+// Consecutive gates with another gate in between should merge
+TEST_F(QCOQuaternionMergeTest, mergeConsecutiveWithGateInBetween) {
+  auto q = builder.allocQubitRegister(2);
+
+  auto q1 = builder.rx(1.0, q[0]);
+  builder.h(q[1]);
+  builder.ry(1.0, q1);
+
+  module = builder.finalize();
+
+  ASSERT_TRUE(runMergePass(module.get()).succeeded());
+  EXPECT_EQ(countOps<RXOp>(), 0);
+  EXPECT_EQ(countOps<HOp>(), 1);
+  EXPECT_EQ(countOps<RYOp>(), 0);
+  EXPECT_EQ(countOps<UOp>(), 1);
+}
+
+// Gates with multiple uses should not be merged but pass should still succeed
+TEST_F(QCOQuaternionMergeTest, nonLinearCodeHandling) {
+  // QCOProgramBuilder does not allow non-linear circuits,
+  // so strings are used
+  const char* mlirCode = R"(
+      module {
+        func.func @multipleUsers() {
+          %0 = qco.alloc : !qco.qubit
+          %cst = arith.constant 1.000000e+00 : f64
+          %1 = qco.ry(%cst) %0 : !qco.qubit -> !qco.qubit
+
+          // %1 is used by BOTH operations - violates linearity!
+          %2 = qco.rz(%cst) %1 : !qco.qubit -> !qco.qubit
+          %3 = qco.rz(%cst) %1 : !qco.qubit -> !qco.qubit
+
+          qco.dealloc %2 : !qco.qubit
+          qco.dealloc %3 : !qco.qubit
+          return
+        }
+      }
+    )";
+
+  module = mlir::parseSourceString<mlir::ModuleOp>(mlirCode, &context);
+  ASSERT_TRUE(module) << "Failed to parse MLIR";
+
+  ASSERT_TRUE(runMergePass(module.get()).succeeded());
+
+  // Gates should remain unchanged (not merged) due to multiple uses
+  EXPECT_EQ(countOps<RZOp>(), 2);
+  EXPECT_EQ(countOps<RYOp>(), 1);
+  EXPECT_EQ(countOps<UOp>(), 0);
+}
+
+// Gates with no final users should still merge and succeed
+TEST_F(QCOQuaternionMergeTest, noUsedGate) {
+  // QCOProgramBuilder does not allow non-linear circuits,
+  // so strings are used
+  const char* mlirCode = R"(
+      module {
+        func.func @multipleUsers() {
+          %0 = qco.alloc : !qco.qubit
+          %cst = arith.constant 1.000000e+00 : f64
+          %1 = qco.ry(%cst) %0 : !qco.qubit -> !qco.qubit
+          %2 = qco.rz(%cst) %1 : !qco.qubit -> !qco.qubit
+          return
+        }
+      }
+    )";
+
+  module = mlir::parseSourceString<mlir::ModuleOp>(mlirCode, &context);
+  ASSERT_TRUE(module) << "Failed to parse MLIR";
+
+  ASSERT_TRUE(runMergePass(module.get()).succeeded());
+
+  // Gates should remain unchanged (not merged) due to multiple uses
+  EXPECT_EQ(countOps<RZOp>(), 1);
+  EXPECT_EQ(countOps<RYOp>(), 1);
+  EXPECT_EQ(countOps<UOp>(), 0);
+}
   EXPECT_EQ(countOps<UOp>(), 1);
   EXPECT_EQ(countOps<RZOp>(), 0);
 }
