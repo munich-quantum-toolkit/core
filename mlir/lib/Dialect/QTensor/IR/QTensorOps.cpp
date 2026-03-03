85906f81467581d43c0b751443c5e07c0f4480dd
Li-ming Bao <ge93vof@tum.de>
2026-03-03 15:38:54 +0100

    refactor dialect structure

--- mlir/lib/Dialect/QTensor/IR/QTensorOps.cpp
+++ mlir/lib/Dialect/QTensor/IR/QTensorOps.cpp
@@ -55,6 +55,94 @@ Operation* QTensorDialect::materializeConstant(OpBuilder& builder,
   return nullptr;
 }
 
+namespace mlir::qtensor {
+
+llvm::SmallBitVector getDroppedDims(ArrayRef<int64_t> reducedShape,
+                                    ArrayRef<OpFoldResult> mixedSizes) {
+  llvm::SmallBitVector droppedDims(mixedSizes.size());
+  int64_t shapePos = reducedShape.size() - 1;
+
+  for (const auto& size : enumerate(llvm::reverse(mixedSizes))) {
+    size_t idx = mixedSizes.size() - size.index() - 1;
+    // Rank-reduced dims must have a static unit dimension.
+    bool isStaticUnitSize =
+        isa<Attribute>(size.value()) &&
+        llvm::cast<IntegerAttr>(cast<Attribute>(size.value())).getInt() == 1;
+
+    if (shapePos < 0) {
+      // There are no more dims in the reduced shape. All remaining sizes must
+      // be rank-reduced dims.
+      assert(isStaticUnitSize && "expected unit dim");
+      droppedDims.set(idx);
+      continue;
+    }
+
+    // Dim is preserved if the size is not a static 1.
+    if (!isStaticUnitSize) {
+      --shapePos;
+      continue;
+    }
+
+    // Dim is preserved if the reduced shape dim is also 1.
+    if (reducedShape[shapePos] == 1) {
+      --shapePos;
+      continue;
+    }
+
+    // Otherwise: Dim is dropped.
+    droppedDims.set(idx);
+  }
+
+  assert(shapePos < 0 && "dimension mismatch");
+  return droppedDims;
+}
+
+LogicalResult produceSliceErrorMsg(SliceVerificationResult result,
+                                   Operation* op,
+                                   RankedTensorType expectedType) {
+  switch (result) {
+  case SliceVerificationResult::Success:
+    return success();
+  case SliceVerificationResult::RankTooLarge:
+    return op->emitError("expected rank to be smaller or equal to ")
+           << "the other rank. ";
+  case SliceVerificationResult::SizeMismatch:
+    return op->emitError("expected type to be ")
+           << expectedType << " or a rank-reduced version. (size mismatch) ";
+  case SliceVerificationResult::ElemTypeMismatch:
+    return op->emitError("expected element type to be ")
+           << expectedType.getElementType();
+  default:
+    llvm_unreachable("unexpected extract_slice op verification result");
+  }
+}
+
+LogicalResult
+foldIdentityOffsetSizeAndStrideOpInterface(OffsetSizeAndStrideOpInterface op,
+                                           ShapedType shapedType) {
+  OpBuilder b(op.getContext());
+  for (OpFoldResult opFold : op.getMixedOffsets()) {
+    if (getConstantIntValue(opFold) != static_cast<int64_t>(0)) {
+      return failure();
+    }
+  }
+  // Rank-reducing noops only need to inspect the leading dimensions:
+  // llvm::zip is appropriate.
+  auto shape = shapedType.getShape();
+  for (auto it : llvm::zip(op.getMixedSizes(), shape)) {
+    if (getConstantIntValue(std::get<0>(it)) != std::get<1>(it)) {
+      return failure();
+    }
+  }
+  for (OpFoldResult opFold : op.getMixedStrides()) {
+    if (getConstantIntValue(opFold) != static_cast<int64_t>(1)) {
+      return failure();
+    }
+  }
+  return success();
+}
+} // namespace mlir::qtensor
+
 //===----------------------------------------------------------------------===//
 // Common Canonicalizers and Folders.
 //===----------------------------------------------------------------------===//
