67e2eff4aa9d7d8684d28390471239b6863b0d89
Matthias Reumann <matthias.reumann@tum.de>
2026-03-20 14:13:04 +0100

    Fix linting

--- mlir/lib/Dialect/QCO/Transforms/Mapping/Mapping.cpp
+++ mlir/lib/Dialect/QCO/Transforms/Mapping/Mapping.cpp
@@ -15,11 +15,9 @@
 #include "mlir/Dialect/QCO/Transforms/Passes.h"
 #include "mlir/Dialect/QCO/Utils/WireIterator.h"
 
-#include <llvm/ADT/DenseMapInfo.h>
 #include <llvm/ADT/STLExtras.h>
 #include <llvm/ADT/SmallVector.h>
 #include <llvm/ADT/TypeSwitch.h>
-#include <llvm/Support/Debug.h>
 #include <llvm/Support/ErrorHandling.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
 #include <mlir/IR/Block.h>
