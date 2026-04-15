e873baa0d6ab09fe6140b98a34e8b5f78d564932
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-04-16 00:27:46 +0200

    Simplify diff

--- mlir/include/mlir/Dialect/QCO/Transforms/Passes.h
+++ mlir/include/mlir/Dialect/QCO/Transforms/Passes.h
@@ -21,6 +21,11 @@ namespace mlir::qco {
 #define GEN_PASS_DECL
 #include "mlir/Dialect/QCO/Transforms/Passes.h.inc" // IWYU pragma: export
 
+//===----------------------------------------------------------------------===//
+// Registration
+//===----------------------------------------------------------------------===//
+
+/// Generate the code for registering passes.
 #define GEN_PASS_REGISTRATION
 #include "mlir/Dialect/QCO/Transforms/Passes.h.inc" // IWYU pragma: export
 
