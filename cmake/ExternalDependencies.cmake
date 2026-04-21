49b0c8442ad0debe56a1f33324c38699fe1dbcb9
Lukas Burgholzer <burgholzer@me.com>
2026-04-21 11:39:08 +0200

    ⬆️ Update QDMI to v1.3.0
    Signed-off-by: Lukas Burgholzer <burgholzer@me.com>

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -98,13 +98,13 @@ if(BUILD_MQT_CORE_TESTS)
 endif()
 
 # cmake-format: off
-set(QDMI_VERSION 1.2.2
+set(QDMI_VERSION 1.3.0
         CACHE STRING "QDMI version")
-set(QDMI_REV "5bcf32f57158beea34d2839a41d218ed46a41516" # v1.2.x
+set(QDMI_REV "0f7e08c58b72800d1022a01cfb618af67b9a9c30" # v1.3.0
         CACHE STRING "QDMI identifier (tag, branch or commit hash)")
 set(QDMI_REPO_OWNER "Munich-Quantum-Software-Stack"
         CACHE STRING "QDMI repository owner (change when using a fork)")
-cmake_dependent_option(QDMI_INSTALL "Install QDMI library" ON "MQT_CORE_INSTALL" OFF)
+cmake_dependent_option(INSTALL_QDMI "Install QDMI library" ON "MQT_CORE_INSTALL" OFF)
 # cmake-format: on
 FetchContent_Declare(
   qdmi
