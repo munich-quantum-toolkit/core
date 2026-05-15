c64fc4a28992ce91301efc3127b2656bf8625dcf
Matthias Reumann <matthias.reumann@tum.de>
2026-05-15 09:54:03 +0200

    Remove qc dialect dependency from mapping test

--- mlir/unittests/Dialect/QCO/Transforms/Mapping/test_mapping.cpp
+++ mlir/unittests/Dialect/QCO/Transforms/Mapping/test_mapping.cpp
@@ -8,9 +8,7 @@
  * Licensed under the MIT License
  */
 
-#include "mlir/Conversion/QCToQCO/QCToQCO.h"
-#include "mlir/Dialect/QC/Builder/QCProgramBuilder.h"
-#include "mlir/Dialect/QC/IR/QCDialect.h"
+#include "mlir/Dialect/QCO/Builder/QCOProgramBuilder.h"
 #include "mlir/Dialect/QCO/IR/QCODialect.h"
 #include "mlir/Dialect/QCO/Transforms/Mapping/Mapping.h"
 #include "mlir/Dialect/QCO/Transforms/Passes.h"
@@ -65,22 +63,20 @@ LogicalResult isExecutable(Region& region, const Edges& coupling) {
   });
 }
 
+DeviceSpec getNineQubitSquareGrid() {
+  const static Edges COUPLING{{0, 3}, {3, 0}, {0, 1}, {1, 0}, {1, 4}, {4, 1},
+                              {1, 2}, {2, 1}, {2, 5}, {5, 2}, {3, 6}, {6, 3},
+                              {3, 4}, {4, 3}, {4, 7}, {7, 4}, {4, 5}, {5, 4},
+                              {5, 8}, {8, 5}, {6, 7}, {7, 6}, {7, 8}, {8, 7}};
+  return std::make_pair(9, COUPLING);
+}
+
 class MappingPassTest : public testing::Test,
                         public testing::WithParamInterface<DeviceSpec> {
-public:
-  static DeviceSpec getNineQubitSquareGrid() {
-    const static Edges COUPLING{{0, 3}, {3, 0}, {0, 1}, {1, 0}, {1, 4}, {4, 1},
-                                {1, 2}, {2, 1}, {2, 5}, {5, 2}, {3, 6}, {6, 3},
-                                {3, 4}, {4, 3}, {4, 7}, {7, 4}, {4, 5}, {5, 4},
-                                {5, 8}, {8, 5}, {6, 7}, {7, 6}, {7, 8}, {8, 7}};
-    return std::make_pair(9, COUPLING);
-  }
-
 protected:
   void SetUp() override {
     DialectRegistry registry;
-    registry.insert<qc::QCDialect, qco::QCODialect, arith::ArithDialect,
-                    func::FuncDialect>();
+    registry.insert<qco::QCODialect, arith::ArithDialect, func::FuncDialect>();
     context = std::make_unique<MLIRContext>();
     context->appendDialectRegistry(registry);
     context->loadAllAvailableDialects();
@@ -89,7 +85,6 @@ protected:
   static void runPass(OwningOpRef<ModuleOp>& program, const DeviceSpec& device,
                       const qco::MappingPassOptions& options) {
     PassManager pm(program->getContext());
-    pm.addPass(createQCToQCO());
     pm.addPass(qco::createMappingPass(device.first, device.second, options));
     auto res = pm.run(*program);
     ASSERT_TRUE(res.succeeded());
@@ -103,31 +98,24 @@ protected:
 TEST_P(MappingPassTest, GHZ) {
   const auto& device = GetParam();
 
-  qc::QCProgramBuilder builder(context.get());
+  qco::QCOProgramBuilder builder(context.get());
   builder.initialize();
 
-  const auto q0 = builder.allocQubit();
-  const auto q1 = builder.allocQubit();
-  const auto q2 = builder.allocQubit();
+  Value q0 = builder.allocQubit();
+  Value q1 = builder.allocQubit();
+  Value q2 = builder.allocQubit();
 
-  builder.h(q0);
-  builder.cx(q0, q1);
-  builder.cx(q0, q2);
+  q0 = builder.h(q0);
+  std::tie(q0, q1) = builder.cx(q0, q1);
+  std::tie(q0, q2) = builder.cx(q0, q2);
 
-  builder.dealloc(q0);
-  builder.dealloc(q1);
-  builder.dealloc(q2);
+  builder.sink(q0);
+  builder.sink(q1);
+  builder.sink(q2);
 
   auto program = builder.finalize();
 
-  const qco::MappingPassOptions options{.nlookahead = 5,
-                                        .alpha = 1,
-                                        .lambda = 0.85,
-                                        .niterations = 2,
-                                        .ntrials = 4,
-                                        .seed = 1337};
-
-  runPass(program, device, options);
+  runPass(program, device, qco::MappingPassOptions{});
   auto entry = *(program->getOps<func::FuncOp>().begin());
   EXPECT_TRUE(isExecutable(entry.getFunctionBody(), device.second).succeeded());
 }
@@ -135,71 +123,78 @@ TEST_P(MappingPassTest, GHZ) {
 TEST_P(MappingPassTest, Sabre) {
   const auto& device = GetParam();
 
-  qc::QCProgramBuilder builder(context.get());
+  qco::QCOProgramBuilder builder(context.get());
   builder.initialize();
 
-  const auto q0 = builder.allocQubit();
-  const auto q1 = builder.allocQubit();
-  const auto q2 = builder.allocQubit();
-  const auto q3 = builder.allocQubit();
-  const auto q4 = builder.allocQubit();
-  const auto q5 = builder.allocQubit();
+  Value q0 = builder.allocQubit();
+  Value q1 = builder.allocQubit();
+  Value q2 = builder.allocQubit();
+  Value q3 = builder.allocQubit();
+  Value q4 = builder.allocQubit();
+  Value q5 = builder.allocQubit();
 
-  builder.h(q0);
-  builder.h(q1);
-  builder.h(q4);
+  q0 = builder.h(q0);
+  q1 = builder.h(q1);
+  q4 = builder.h(q4);
 
-  builder.z(q0);
-  builder.cx(q1, q2);
-  builder.cx(q4, q5);
+  q0 = builder.z(q0);
+  std::tie(q1, q2) = builder.cx(q1, q2);
+  std::tie(q4, q5) = builder.cx(q4, q5);
 
-  builder.cx(q0, q1);
+  std::tie(q0, q1) = builder.cx(q0, q1);
 
-  builder.h(q0);
-  builder.y(q1);
-  builder.cx(q0, q1);
+  q0 = builder.h(q0);
+  q1 = builder.y(q1);
+  std::tie(q0, q1) = builder.cx(q0, q1);
 
-  builder.cx(q2, q3);
+  std::tie(q2, q3) = builder.cx(q2, q3);
 
-  builder.h(q2);
-  builder.h(q3);
+  q2 = builder.h(q2);
+  q3 = builder.h(q3);
 
-  builder.cx(q1, q2);
-  builder.cx(q3, q5);
+  std::tie(q1, q2) = builder.cx(q1, q2);
+  std::tie(q3, q5) = builder.cx(q3, q5);
 
-  builder.z(q3);
+  q3 = builder.z(q3);
 
-  builder.cx(q3, q4);
+  std::tie(q3, q4) = builder.cx(q3, q4);
 
-  builder.cx(q3, q0);
+  std::tie(q3, q0) = builder.cx(q3, q0);
 
-  builder.barrier({q0, q1, q2, q3, q4, q5});
-  builder.measure(q0);
-  builder.measure(q1);
-  builder.measure(q2);
-  builder.measure(q3);
-  builder.measure(q4);
-  builder.measure(q5);
+  ValueRange out = builder.barrier({q0, q1, q2, q3, q4, q5});
+  q0 = out[0];
+  q1 = out[1];
+  q2 = out[2];
+  q3 = out[3];
+  q4 = out[4];
+  q5 = out[5];
 
-  builder.dealloc(q0);
-  builder.dealloc(q1);
-  builder.dealloc(q2);
-  builder.dealloc(q3);
-  builder.dealloc(q4);
-  builder.dealloc(q5);
+  Value c0;
+  Value c1;
+  Value c2;
+  Value c3;
+  Value c4;
+  Value c5;
+
+  std::tie(q0, c0) = builder.measure(q0);
+  std::tie(q1, c1) = builder.measure(q1);
+  std::tie(q2, c2) = builder.measure(q2);
+  std::tie(q3, c3) = builder.measure(q3);
+  std::tie(q4, c4) = builder.measure(q4);
+  std::tie(q5, c5) = builder.measure(q5);
+
+  builder.sink(q0);
+  builder.sink(q1);
+  builder.sink(q2);
+  builder.sink(q3);
+  builder.sink(q4);
+  builder.sink(q5);
 
   auto program = builder.finalize();
-  const qco::MappingPassOptions options{.nlookahead = 1,
-                                        .alpha = 1,
-                                        .lambda = 0.85,
-                                        .niterations = 2,
-                                        .ntrials = 1,
-                                        .seed = 42};
-  runPass(program, device, options);
+  runPass(program, device, qco::MappingPassOptions{});
   auto entry = *(program->getOps<func::FuncOp>().begin());
   EXPECT_TRUE(isExecutable(entry.getFunctionBody(), device.second).succeeded());
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    NineQubitSquareGrid, MappingPassTest,
-    testing::Values(MappingPassTest::getNineQubitSquareGrid()));
+INSTANTIATE_TEST_SUITE_P(NineQubitSquareGrid, MappingPassTest,
+                         testing::Values(getNineQubitSquareGrid()));
