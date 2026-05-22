659f527e5f68de4835b7c1ae39bf125ea35adf25
Li-ming Bao <ge93vof@tum.de>
2026-05-22 11:42:55 +0200

    fix linter issues

--- mlir/lib/Conversion/QCToQIR/QIRBase/QCToQIRBase.cpp
+++ mlir/lib/Conversion/QCToQIR/QIRBase/QCToQIRBase.cpp
@@ -27,9 +27,7 @@
 #include <mlir/Dialect/LLVMIR/LLVMDialect.h>
 #include <mlir/Dialect/LLVMIR/LLVMTypes.h>
 #include <mlir/Dialect/MemRef/IR/MemRef.h>
-#include <mlir/Dialect/Utils/StaticValueUtils.h>
 #include <mlir/IR/BuiltinAttributes.h>
-#include <mlir/IR/BuiltinOps.h>
 #include <mlir/IR/BuiltinTypes.h>
 #include <mlir/IR/MLIRContext.h>
 #include <mlir/IR/OpDefinition.h>
