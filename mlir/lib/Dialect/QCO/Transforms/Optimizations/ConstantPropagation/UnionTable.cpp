4ef17bb2aca750097eb83b2c01e83380eefec3d9
Remme, Lian (lirem101) <lirem101@hhu.de>
2026-06-19 11:28:57 +0200

    :construction: Added UnionTable methods

--- mlir/lib/Dialect/QCO/Transforms/Optimizations/ConstantPropagation/UnionTable.cpp
+++ mlir/lib/Dialect/QCO/Transforms/Optimizations/ConstantPropagation/UnionTable.cpp
@@ -12,25 +12,93 @@
 #define MQT_CORE_UNIONTABLE
 #include "mlir/Dialect/QCO/Transforms/Optimizations/ConstantPropagation/UnionTable.hpp"
 
+#include "mlir/Dialect/QCO/IR/QCOInterfaces.h"
+#include "mlir/Dialect/QCO/IR/QCOOps.h"
+
 namespace mlir::qco {
 
 UnionTable::UnionTable(std::size_t maxNonzeroAmplitudes,
-                       std::size_t maximumHybridEntries) {}
+                       std::size_t maximumHybridEntries)
+    : maxNonzeroAmplitudes(maxNonzeroAmplitudes),
+      maximumHybridEntries(maximumHybridEntries) {}
 
-UnionTable::~UnionTable() {}
+UnionTable::~UnionTable() = default;
 
-void UnionTable::print(std::ostream& os) const {}
+void UnionTable::print(std::ostream& os) const { os << this->toString(); }
 
-std::string UnionTable::toString() const {}
+std::string UnionTable::toString() const {
+  std::string result;
+  for (const auto& entry : entries) {
+    std::vector<unsigned int> qubitIndices;
+    for (const auto& [_, q] : entry.participatingQubits) {
+      qubitIndices.push_back(q);
+    }
+    std::ranges::sort(qubitIndices, std::greater<int>());
+    result += "Qubits: ";
+    for (const auto qit : qubitIndices) {
+      result += std::to_string(qit);
+    }
+    result += ", HybridStates: {";
+    bool first = true;
+    for (HybridState const& hs : entry.states) {
+      if (!first) {
+        result += " ";
+      }
+      first = false;
+      result += hs.toString();
+    }
+    result += "}\n";
+  }
+  return result;
+}
 
-bool UnionTable::areStatesAllTop() {}
+bool UnionTable::areStatesAllTop() const { return allTop; }
 
 void UnionTable::propagateGate(Operation* gate, std::span<Value> targets,
                                std::span<Value> newQuantumTargets,
                                std::span<Value> ctrlsQuantum,
                                std::span<Value> posCtrlsClassical,
                                std::span<Value> negCtrlsClassical,
-                               std::vector<Value> params) {}
+                               std::vector<Value> params) {
+  if (isa<SWAPOp>(*gate) && ctrlsQuantum.empty() && posCtrlsClassical.empty() &&
+      negCtrlsClassical.empty()) {
+    applySwapGate(targets, newQuantumTargets);
+    return;
+  }
+
+  const std::set<UnionTableEntry> participatingEntries =
+      collectParticipatingEntries(targets, ctrlsQuantum, posCtrlsClassical,
+                                  negCtrlsClassical, params);
+
+  try {
+    unifyEntries(participatingEntries);
+  } catch (std::domain_error&) {
+    putEntriesToTop(participatingEntries);
+    replaceValuesGlobally(targets, newQuantumTargets);
+    return;
+  }
+
+  auto ute = valuesToEntries.at(*targets.begin());
+  std::vector<unsigned int> targetQubitIndices;
+  std::vector<unsigned int> ctrlQubitIndices;
+  for (auto const q : targets) {
+    targetQubitIndices.push_back(ute->participatingQubits.at(q));
+  }
+  for (auto const q : ctrlsQuantum) {
+    ctrlQubitIndices.push_back(ute->participatingQubits.at(q));
+  }
+
+  for (auto hs : ute->states) {
+    try {
+      hs.propagateGate(gate, targetQubitIndices, ctrlQubitIndices,
+                       posCtrlsClassical, negCtrlsClassical, params);
+    } catch (std::domain_error&) {
+      putEntriesToTop(participatingEntries);
+      break;
+    }
+  }
+  replaceValuesGlobally(targets, newQuantumTargets);
+}
 
 void UnionTable::propagateMeasurement(Value quantumTarget,
                                       Value newQuantumValue,
