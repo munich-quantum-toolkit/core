df57fcfa7aee6210300e93af22f5762cf1ad3203
Remme, Lian (lirem101) <lirem101@hhu.de>
2026-07-15 10:28:59 +1000

    :poop: Find segfault on macos

--- mlir/unittests/Dialect/QCO/Transforms/Optimizations/test_qco_constant_propagation.cpp
+++ mlir/unittests/Dialect/QCO/Transforms/Optimizations/test_qco_constant_propagation.cpp
@@ -225,7 +225,6 @@ TEST_F(QCOConstantPropagationTest, testUnsatisfiableHybridCombination) {
       });
   // programBuilder.y(qRange01[1]);
   module = programBuilder.finalize();
-  std::cout << "After program finalization." << std::endl;
 
   auto qRef = referenceBuilder.allocQubitRegister(2);
   qRef[0] = referenceBuilder.h(qRef[0]);
@@ -234,7 +233,6 @@ TEST_F(QCOConstantPropagationTest, testUnsatisfiableHybridCombination) {
   referenceBuilder.measure(qRef0);
   // referenceBuilder.y(qRef1);
   reference = referenceBuilder.finalize();
-  std::cout << "After reference finalization." << std::endl;
 
   ASSERT_TRUE(runConstantPropagationPass(module.get()).succeeded());
 
