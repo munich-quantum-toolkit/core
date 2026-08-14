1edae74c5af3debdb5725e040d8bdc6a5fe4b380
Simon Hofmann <simon.t.hofmann@tum.de>
2026-08-14 13:25:46 +0200

    ♻️ Share QCO DD unitary matrix validation

--- mlir/lib/Dialect/QCO/Utils/DDFunctionality.cpp
+++ mlir/lib/Dialect/QCO/Utils/DDFunctionality.cpp
@@ -261,11 +261,11 @@ template <typename StateDD>
 static LogicalResult applyUnitaryMatrix(UnitaryOpInterface unitary,
                                         WalkState& walk, StateDD& state) {
   Operation* op = unitary.getOperation();
+  if (!unitary.hasCompileTimeKnownUnitaryMatrix()) {
+    return unitary.emitError()
+           << "unitary must have a compile-time constant matrix";
+  }
   if (auto gphase = dyn_cast<GPhaseOp>(op)) {
-    if (!gphase.hasCompileTimeKnownUnitaryMatrix()) {
-      return gphase.emitError()
-             << "unitary must have a compile-time constant matrix";
-    }
     const auto theta = *utils::valueToDouble(gphase.getTheta());
     auto id = dd::Package::makeIdent();
     id.w = walk.dd->cn.lookup(std::cos(theta), std::sin(theta));
