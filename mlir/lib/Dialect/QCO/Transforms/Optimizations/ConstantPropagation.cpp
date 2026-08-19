08ed321f8c8fc68e0c1b3c32b8bbe6b234d16a22
Remme, Lian (lirem101) <lirem101@hhu.de>
2026-08-19 18:18:38 +0200

    :memo: Removed redundant copyright header

--- mlir/lib/Dialect/QCO/Transforms/Optimizations/ConstantPropagation.cpp
+++ mlir/lib/Dialect/QCO/Transforms/Optimizations/ConstantPropagation.cpp
@@ -8,16 +8,6 @@
  * Licensed under the MIT License
  */
 
-/*
- * Copyright (c) 2023 - 2026 Chair for Design Automation, TUM
- * Copyright (c) 2025 - 2026 Munich Quantum Software Company GmbH All rights
- * reserved.
- *
- * SPDX-License-Identifier: MIT
- *
- * Licensed under the MIT License
- */
-
 #include "mlir/Dialect/QCO/IR/QCOInterfaces.h"
 #include "mlir/Dialect/QCO/IR/QCOOps.h"
 #include "mlir/Dialect/QCO/Transforms/Optimizations/ConstantPropagation/UnionTable.hpp"
