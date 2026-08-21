8df43d24125bb840f97a9f59fe77e4e09772b234
matthias <matthias.reumann@tum.de>
2026-08-21 15:02:16 +0200

    Fix UB

--- mlir/lib/Dialect/QCO/Transforms/Mapping/Mapping.cpp
+++ mlir/lib/Dialect/QCO/Transforms/Mapping/Mapping.cpp
@@ -1048,7 +1048,7 @@ private:
               const auto prog1 = infos.lookupProgram(i1);
               const IndexPairType gate = std::minmax(prog0, prog1);
 
-              if (!llvm::is_contained(prev, gate)) {
+              if (!is_contained(prev, gate)) {
                 window.emplace_back(gate);
                 if (window.size() == 1 + nlookahead) {
                   return WalkResult::interrupt();
@@ -1062,6 +1062,8 @@ private:
 
           prev.swap(next);
           next.clear();
+
+          return WalkResult::advance();
         });
 
     return window;
